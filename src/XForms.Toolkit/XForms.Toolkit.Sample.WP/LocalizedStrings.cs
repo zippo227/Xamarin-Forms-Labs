52f24800b55b43b6e9a7ba2274464ba1846e1d89
Rui Marinho <me@ruimarinho.net>
2014-06-04 23:58:07 +0100

    Updated with windows phone project

---
+++ src/XForms.Toolkit/XForms.Toolkit.Sample.WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using XForms.Toolkit.Sample.WP.Resources;
+
+namespace XForms.Toolkit.Sample.WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ src/XForms.Toolkit/XForms.Toolkit.WP/Services/TextToSpeechService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.Phone.Speech.Synthesis;
+using Xamarin.Forms;
+using XForms.Toolkit.Services;
+using XForms.Toolkit.WP.Services;
+
+
+[assembly: Dependency(typeof(TextToSpeechService))]
+namespace XForms.Toolkit.WP.Services
+{
+    public class TextToSpeechService : ITextToSpeechService
+    {
+        SpeechSynthesizer synth;
+
+        public TextToSpeechService()
+        {
+            synth = new SpeechSynthesizer();
+        }
+
+
+        public async void Speak(string text)
+        {
+            await synth.SpeakTextAsync(text);
+        }
+    }
+}
