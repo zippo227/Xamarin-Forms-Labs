483ee9292a4455784086ae3dadd19baf55cdec1e
Rui Marinho <rui@mbprui.home>
2014-06-05 23:41:56 +0100

    Update App

---
+++ samples/XForms.Toolkit.Sample/App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using XForms.Toolkit.Controls;
+using XForms.Toolkit.Sample.Pages.Controls;
+using System.Diagnostics;
+
+namespace XForms.Toolkit.Sample
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			var mainPage = new ExtendedTabbedPage ();
+			mainPage.CurrentPageChanged +=
+				() => Debug.WriteLine(string.Format("ExtendedTabbedPage CurrentPageChanged {0}",mainPage.CurrentPage.Title));
+
+			var controls = new CarouselPage ();
+			controls.Title = "Controls";
+			controls.Children.Add (new CalendarPage ());
+
+			var services = new CarouselPage ();
+			services.Title = "Services";
+			services.Children.Add (new TextToSpeechPage ());
+
+			var buttons = new CarouselPage ();
+			buttons.Title = "Buttons";
+			buttons.Children.Add (new ButtonPage ());
+
+			mainPage.Children.Add (controls);
+			mainPage.Children.Add (services);
+			mainPage.Children.Add (buttons);
+
+			return mainPage;
+		}
+	}
+}
+
