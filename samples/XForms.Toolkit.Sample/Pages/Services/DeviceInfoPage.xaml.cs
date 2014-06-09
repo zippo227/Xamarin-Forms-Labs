4bf9a9b91d1b9f2d165100b78a6d9749d7c72970
rmarinho <rui@mbprui.home>
2014-06-10 00:18:12 +0100

    Update sample with DeviceInfoPage

---
+++ samples/XForms.Toolkit.Sample/Pages/Services/DeviceInfoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XForms.Toolkit.Sample
+{	
+	public partial class DeviceInfoPage : ContentPage
+	{	
+		public DeviceInfoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
