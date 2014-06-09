0556f43a3064991f57a659dfc41f15adde7c67c5
Kevin E Ford <Bowman74@hotmail.com>
2014-06-09 17:11:47 -0400

    #19, more changes for iOS buttons.

--- src/XForms.Toolkit/XForms.Toolkit.iOS/Controls/ImageButton/ImageButtonRenderer.cs
+++ src/XForms.Toolkit/XForms.Toolkit.iOS/Controls/ImageButton/ImageButtonRenderer.cs
@@ -1,22 +1,18 @@
-using System.Collections;
-using System.Drawing;
-using System.Security.Cryptography;
 using MonoTouch.UIKit;
 using System;
+using System.Drawing;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 using XForms.Toolkit.Controls;
 using XForms.Toolkit.Enums;
 using XForms.Toolkit.iOS.Controls.ImageButton;
 
-
-/// WIP - Not yet ready for use.
 [assembly: ExportRenderer(typeof(ImageButton), typeof(ImageButtonRenderer))]
 namespace XForms.Toolkit.iOS.Controls.ImageButton
 {
     public class ImageButtonRenderer : ButtonRenderer
     {
-        private const int padding = 10;
+        private const int controlPadding = 5;
 
         protected override void OnModelSet(VisualElement model)
         {
@@ -26,77 +22,64 @@ namespace XForms.Toolkit.iOS.Controls.ImageButton
             if (imageButton != null && targetButton != null && !String.IsNullOrEmpty(imageButton.Image))
             {
                 SetImage(imageButton.Image, imageButton.ImageWidthRequest, imageButton.ImageHeightRequest, targetButton);
-                var label = new UILabel(new RectangleF(0, 0, 100, 30));
-                label.Text = imageButton.Text;
-                targetButton.SetTitle("", UIControlState.Normal);
-                targetButton.AddSubview(label);
-                //targetButton.TitleLabel.TextAlignment = UITextAlignment.Center;
 
-
-                //switch (imageButton.Orientation)
-                //{
-                //    case ImageOrientation.ImageToLeft:
-                //        AlignToLeft(imageButton.ImageWidthRequest, targetButton);
-                //        break;
-                //    case ImageOrientation.ImageToRight:
-                //        AlignToRight(imageButton.ImageWidthRequest, targetButton);
-                //        break;
-                //    case ImageOrientation.ImageOnTop:
-                //        AlignToTop(imageButton.ImageHeightRequest, targetButton);
-                //        break;
-                //}
-                
-                targetButton.SizeToFit();
-                //targetButton.ImageEdgeInsets = new UIEdgeInsets(10, 10, 10, ((int)page.WidthRequest - page.ImageWidthRequest));
-                //targetButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Left;
-                //targetButton.ImageRectForContentRect(new RectangleF(0, 10, 100, 100));
-                //targetButton.TitleRectForContentRect(new RectangleF(20, 10, 50, 20));
+                switch (imageButton.Orientation)
+                {
+                    case ImageOrientation.ImageToLeft:
+                        AlignToLeft(imageButton.ImageWidthRequest, targetButton);
+                        break;
+                    case ImageOrientation.ImageToRight:
+                        AlignToRight(imageButton.ImageWidthRequest, targetButton);
+                        break;
+                    case ImageOrientation.ImageOnTop:
+                        AlignToTop(imageButton.ImageHeightRequest, imageButton.ImageWidthRequest, targetButton);
+                        break;
+                    case ImageOrientation.ImageOnBottom:
+                        AlignToBottom(imageButton.ImageHeightRequest, imageButton.ImageWidthRequest, targetButton);
+                        break;
+                }                
             }
         }
 
         private void AlignToLeft(int widthRequest, UIButton targetButton)
         {
             targetButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Left;
+            targetButton.TitleLabel.TextAlignment = UITextAlignment.Left;
 
-            var titleInsets = new UIEdgeInsets(0, (widthRequest + padding), 0, -1 * (widthRequest + padding));
-            var contentInsets = new UIEdgeInsets(0, 15, 0, 15);
-            //var imageInsets = new UIEdgeInsets(0, widthRequest, 0, -1 * (targetButton.Bounds.Width - page.ImageWidthRequest));
-            //targetButton.ImageEdgeInsets = imageInsets;
-            contentInsets.Right += titleInsets.Left - titleInsets.Right;
+            var titleInsets = new UIEdgeInsets(0, controlPadding, 0, -1 * (controlPadding));
 
             targetButton.TitleEdgeInsets = titleInsets;
-            targetButton.ContentEdgeInsets = contentInsets;
-            targetButton.SizeToFit();
         }
 
         private void AlignToRight(int widthRequest, UIButton targetButton)
         {
             targetButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Right;
+            targetButton.TitleLabel.TextAlignment = UITextAlignment.Right;
 
-            var titleInsets = new UIEdgeInsets(0, -1 + (2 * widthRequest + padding), 0, (2 * widthRequest + padding));
-            var contentInsets = new UIEdgeInsets(0, 15, 0, 15);
-            //var imageInsets = new UIEdgeInsets(0, widthRequest, 0, -1 * (targetButton.Bounds.Width - page.ImageWidthRequest));
-            //targetButton.ImageEdgeInsets = imageInsets;
-            contentInsets.Left += titleInsets.Right - titleInsets.Left;
+            var titleInsets = new UIEdgeInsets(0, -1 * (2 * widthRequest + controlPadding), 0, (2 * widthRequest + controlPadding));
 
             targetButton.TitleEdgeInsets = titleInsets;
-            targetButton.ContentEdgeInsets = contentInsets;
             targetButton.SizeToFit();
         }
 
-        private void AlignToTop(int heightRequest, UIButton targetButton)
+        private void AlignToTop(int heightRequest, int widthRequest, UIButton targetButton)
         {
             targetButton.VerticalAlignment = UIControlContentVerticalAlignment.Top;
+            targetButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
+            targetButton.TitleLabel.TextAlignment = UITextAlignment.Center;
 
-            var titleInsets = new UIEdgeInsets(heightRequest + padding, 0, -1 * (heightRequest + padding), 0);
-            var contentInsets = new UIEdgeInsets(15, 0, 15, 0);
-            //var imageInsets = new UIEdgeInsets(0, widthRequest, 0, -1 * (targetButton.Bounds.Width - page.ImageWidthRequest));
-            //targetButton.ImageEdgeInsets = imageInsets;
-            contentInsets.Bottom += titleInsets.Top - titleInsets.Bottom;
+            var titleInsets = new UIEdgeInsets(heightRequest + controlPadding, -1 * (widthRequest - controlPadding), -1 * (heightRequest + controlPadding), (widthRequest - controlPadding));
             targetButton.TitleEdgeInsets = titleInsets;
-            targetButton.ContentEdgeInsets = contentInsets;
-            targetButton.Frame = new RectangleF(0, 0, 10, 10);
-            //targetButton.SizeToFit();
+        }
+
+        private void AlignToBottom(int heightRequest, int widthRequest, UIButton targetButton)
+        {
+            targetButton.VerticalAlignment = UIControlContentVerticalAlignment.Bottom;
+            targetButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
+            targetButton.TitleLabel.TextAlignment = UITextAlignment.Center;
+
+            var titleInsets = new UIEdgeInsets(-1 * heightRequest, -1 * (widthRequest - controlPadding), heightRequest, (widthRequest - controlPadding));
+            targetButton.TitleEdgeInsets = titleInsets;
         }
 
         private void SetImage(string imageName, int widthRequest, int heightRequest, UIButton targetButton)
@@ -107,13 +90,10 @@ namespace XForms.Toolkit.iOS.Controls.ImageButton
             image.Draw(new RectangleF(0, 0, widthRequest, heightRequest));
             var resultImage = UIGraphics.GetImageFromCurrentImageContext();
             UIGraphics.EndImageContext();
-            var resizableImage =
-                resultImage.CreateResizableImage(new UIEdgeInsets(0, 0, widthRequest, heightRequest));
+            var resizableImage = resultImage.CreateResizableImage(new UIEdgeInsets(0, 0, widthRequest, heightRequest));
 
-            var imageView = new UIImageView(resizableImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal));
-            //targetButton.SetImage(resizableImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal),
-            //    UIControlState.Normal);
-            targetButton.AddSubview(imageView);
+            targetButton.SetImage(resizableImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal),
+                UIControlState.Normal);
         }
     }
 }
