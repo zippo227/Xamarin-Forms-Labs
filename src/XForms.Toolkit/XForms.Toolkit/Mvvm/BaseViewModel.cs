d58e88ae9f06c384078a7edce99dc83757f01a2b
Jason Smith <jassmith@gmail.com>
2014-06-12 15:10:36 -0700

    Add simple ViewModel based NavigationService

---
+++ src/XForms.Toolkit/XForms.Toolkit/Mvvm/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XForms.Toolkit.Mvvm
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public ViewModelNavigation Navigation { get; set; }
+		protected BaseViewModel ()
+		{
+			
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+				handler (this, new PropertyChangedEventArgs (propertyName));
+		}
+	}
+}
