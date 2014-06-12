313796ac62de6ea31de41c128fb094b66dbb6a5f
Sami M. Kallio <code.kallio@gmail.com>
2014-06-12 18:52:02 -0400

    Added serializer interfaces and extensions
    Serialization interfaces for stream, string and byte serializers.
    Plugins for Json.NET, ServiceStack, ProtoBuffer to follow. These are
    needed for SQLite.Net caching so we can store serialized objects to
    data tables and easily support BLOB fields for complex objects.

---
+++ src/XForms.Toolkit/XForms.Toolkit/Services/Serialization/IStreamSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XForms.Toolkit.Services.Serialization
+{
+    /// <summary>
+    /// Defines a generic interface for stream serializer
+    /// </summary>
+    public interface IStreamSerializer
+    {
+        /// <summary>
+        /// Serializes object to a stream
+        /// </summary>
+        /// <typeparam name="T">Type of object to serialize</typeparam>
+        /// <param name="obj">Object to serialize</param>
+        /// <param name="stream">Stream to serialize to</param>
+        void Serialize<T>(T obj, Stream stream);
+
+        /// <summary>
+        /// Deserializes stream into an object
+        /// </summary>
+        /// <typeparam name="T">Type of object to serialize to</typeparam>
+        /// <param name="stream">Stream to deserialize from</param>
+        /// <returns>Object of type T</returns>
+        T Deserialize<T>(Stream stream);
+    }
+}
---
+++ src/XForms.Toolkit/XForms.Toolkit/Services/Serialization/SerializationFormat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XForms.Toolkit.Services.Serialization
+{
+    /// <summary>
+    /// Serialization format type
+    /// </summary>
+    public enum SerializationFormat
+    {
+        /// <summary>
+        /// Custom undefined format
+        /// </summary>
+        Custom,
+
+        /// <summary>
+        /// JSON format
+        /// </summary>
+        Json,
+
+        /// <summary>
+        /// XML format
+        /// </summary>
+        Xml,
+
+        /// <summary>
+        /// ProtoBuffer format
+        /// </summary>
+        ProtoBuffer,
+
+        /// <summary>
+        /// Custom binary
+        /// </summary>
+        Binary
+    }
+}
