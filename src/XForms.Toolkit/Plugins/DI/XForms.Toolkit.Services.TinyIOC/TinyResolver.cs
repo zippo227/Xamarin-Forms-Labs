920110183ae2851e0f027a043915976f259ff01d
Sami M. Kallio <code.kallio@gmail.com>
2014-06-14 16:34:07 -0400

    Added TinyIOC DI resolver
    Will add simple DI container implementation but 3rd party resolvers
    usually are better to just configure using their own API and then wrap
    them around IResolver interface.

---
+++ src/XForms.Toolkit/Plugins/DI/XForms.Toolkit.Services.TinyIOC/TinyResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyIoC;
+
+namespace XForms.Toolkit.Services.TinyIOC
+{
+    public class TinyResolver : IResolver
+    {
+        private readonly TinyIoCContainer container;
+
+        public TinyResolver(TinyIoCContainer container)
+        {
+            this.container = container;
+        }
+
+        #region IResolver Members
+
+        public T Resolve<T>() where T : class
+        {
+            return this.container.Resolve<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            return this.container.Resolve(type);
+        }
+
+        public IEnumerable<T> ResolveAll<T>() where T : class
+        {
+            return this.container.ResolveAll<T>();
+        }
+
+        public IEnumerable<object> ResolveAll(Type type)
+        {
+            return this.container.ResolveAll(type);
+        }
+
+        #endregion
+    }
+}
