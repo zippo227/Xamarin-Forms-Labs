684d74ba079b87436a5a628a3b07bb0be249e100
Sara Silva <saramgsilva@gmail.com>
2014-06-15 23:08:15 +0100

    Solve an issue in GetPositionAsync for WP8.0 (#34)

--- src/XForms.Toolkit/XForms.Toolkit.WP/Services/Geolocation/Geolocator.cs
+++ src/XForms.Toolkit/XForms.Toolkit.WP/Services/Geolocation/Geolocator.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 using XForms.Toolkit;
 
+[assembly: Dependency(typeof(XForms.Toolkit.Services.Geolocation.Geolocator))]
 namespace XForms.Toolkit.Services.Geolocation
 {
     using Windows.Devices.Geolocation;
@@ -74,24 +77,35 @@ namespace XForms.Toolkit.Services.Geolocation
             return await GetPositionAsync(timeout);
         }
 
-        public async Task<Position> GetPositionAsync(System.Threading.CancellationToken cancelToken)
+        public async Task<Position> GetPositionAsync(CancellationToken cancelToken)
         {
-            throw new NotImplementedException();
+            return await GetPositionAsync(cancelToken, false);
         }
 
-        public Task<Position> GetPositionAsync(System.Threading.CancellationToken cancelToken, bool includeHeading)
+        public Task<Position> GetPositionAsync(CancellationToken cancelToken, bool includeHeading)
         {
-            throw new NotImplementedException();
+            return GetPositionAsync(Timeout.Infinite, cancelToken, includeHeading);
         }
 
-        public Task<Position> GetPositionAsync(int timeout, System.Threading.CancellationToken cancelToken)
+        public Task<Position> GetPositionAsync(int timeout, CancellationToken cancelToken)
         {
-            throw new NotImplementedException();
+            return GetPositionAsync(timeout, cancelToken, false);
         }
 
-        public Task<Position> GetPositionAsync(int timeout, System.Threading.CancellationToken cancelToken, bool includeHeading)
+        public Task<Position> GetPositionAsync(int timeout, CancellationToken cancelToken, bool includeHeading)
         {
-            throw new NotImplementedException();
+
+            if (timeout <= 0 && timeout != Timeout.Infinite)
+            {
+                throw new ArgumentOutOfRangeException("timeout", "timeout must be greater than or equal to 0");
+            }
+
+            var tcs = new TaskCompletionSource<Position>(locator);
+            locator.ReportInterval = 5;
+            locator.PositionChanged += (sender, args) => tcs.TrySetResult(args.Position.Coordinate.GetPosition());
+            locator.GetGeopositionAsync(TimeSpan.MaxValue, TimeSpan.FromMilliseconds(timeout));
+           
+            return tcs.Task;
         }
 
         public void StartListening(uint minTime, double minDistance)
