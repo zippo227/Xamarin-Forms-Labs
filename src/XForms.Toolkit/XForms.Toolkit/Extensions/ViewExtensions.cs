3b1c4a79092dce94f30528375f9ae394d0a099f4
Sami M. Kallio <code.kallio@gmail.com>
2014-06-21 16:28:11 -0400

    Added view extensions for setting width/height
    Draft for extended content view added as well to possibly create
    bindable width/height request in inches.

---
+++ src/XForms.Toolkit/XForms.Toolkit/Extensions/ViewExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XForms.Toolkit.Services;
+
+namespace XForms.Toolkit.Extensions
+{
+    public static class ViewExtensions
+    {
+        private static IDisplay Display
+        {
+            get
+            {
+                return Resolver.Resolve<IDisplay>() ?? 
+                    Resolver.Resolve<IDevice>().Display;
+            }
+        }
+
+        private static double? widthInInches;
+        private static double? heightInInches;
+
+        private static double WidthInInches
+        {
+            get
+            {
+                if (widthInInches.HasValue)
+                {
+                    return widthInInches.Value;
+                }
+                
+                widthInInches = Display.WidthRequestInInches(1);
+                return widthInInches.Value;
+            }
+        }
+
+        public static double GetWidthRequestInInches(this View view)
+        {
+            return view.WidthRequest / WidthInInches;
+        }
+
+        public static void SetWidthRequestInInches(this View view, double inches)
+        {
+            view.WidthRequest = inches * WidthInInches;
+        }
+
+        public static double GetHeightRequestInInches(this View view)
+        {
+            return view.HeightRequest / WidthInInches;
+        }
+
+        public static void SetHeightRequestInInches(this View view, double inches)
+        {
+            view.HeightRequest = inches * WidthInInches;
+        }
+    }
+}
