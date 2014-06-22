388d523325837de1aa931b06c8b4462b2df141cc
Rui Marinho <me@ruimarinho.net>
2014-06-22 03:26:50 +0100

    Renaming folders

---
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.WP/Extensions/VectorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms.Labs.Helpers;
+
+namespace Xamarin.Forms.Labs
+{
+    public static class VectorExtensions
+    {
+        public static Vector3 AsVector3(this Microsoft.Xna.Framework.Vector3 reading)
+        {
+            return new Vector3(reading.X, reading.Y, reading.Z);
+        }
+    }
+}
