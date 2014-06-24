a110c7ef0eeb9bf7dacd0a4efb86512bdb45315d
Rui Marinho <me@ruimarinho.net>
2014-06-24 12:55:00 +0100

    adding nuget spect to cryptography plugin

--- src/Xamarin.Forms.Labs/Plugins/Cryptography/Xamarin.Forms.Labs.Cryptography/Properties/AssemblyInfo.cs
+++ src/Xamarin.Forms.Labs/Plugins/Cryptography/Xamarin.Forms.Labs.Cryptography/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Xamarin.Forms.Labs.Cryptography")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.Forms.Labs.Cryptography")]
+[assembly: AssemblyCompany("Xamarin Forms Labs Team")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
