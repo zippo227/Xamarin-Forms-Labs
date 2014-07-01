6b709d04ab32429e166eb49b6b39c0a40a7c48d1
Kevin E Ford <Bowman74@hotmail.com>
2014-07-01 08:28:58 -0400

    #28, made some changes to be in compliance with StyleCop rules.

--- samples/Xamarin.Forms.Labs.Sample/ViewModel/CacheServiceViewModel.cs
+++ samples/Xamarin.Forms.Labs.Sample/ViewModel/CacheServiceViewModel.cs
@@ -1,98 +1,144 @@
 ﻿using System;
-using Xamarin.Forms.Labs.Mvvm;
-using Xamarin.Forms.Labs.Services;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Xamarin.Forms.Labs.Mvvm;
+using Xamarin.Forms.Labs.Services;
 
 namespace Xamarin.Forms.Labs.Sample
 {
-	public class CacheServiceViewModel : ViewModel
-	{
-		private readonly ISimpleCache _cacheService; 
+    /// <summary>
+    /// Sample ViewModel for the CacheService.
+    /// </summary>
+    public class CacheServiceViewModel : ViewModel
+    {
+        /// <summary>
+        /// Key used in the cache.
+        /// </summary>
+        private const string DemoKey = "test-key";
 
-		private string _demoKey = "test-key";
-		public CacheServiceViewModel ()
-		{
-			try {
-				_cacheService = Resolver.Resolve<ISimpleCache> ();
+        /// <summary>
+        /// Holds a reference to the CacheService.
+        /// </summary>
+        private readonly ISimpleCache cacheService;
 
-			} catch (Exception ex) {
-				
-			}
-			CheckKeyAndDownloadNewContent ();
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheServiceViewModel"/> class.
+        /// </summary>
+        public CacheServiceViewModel()
+        {
+            cacheService = Resolver.Resolve<ISimpleCache>();
+            CheckKeyAndDownloadNewContent();
+        }
 
-		private void CheckKeyAndDownloadNewContent(){
-			if (_cacheService == null)
-				throw new ArgumentNullException("_cacheService", new Exception("Native SimpleCache implementation wasn't found."));
+        /// <summary>
+        /// Downloads items to the cache based on a key.
+        /// </summary>
+        private void CheckKeyAndDownloadNewContent()
+        {
+            if (cacheService == null)
+            {
+                throw new ArgumentNullException(
+                    "_cacheService",
+                    new Exception("Native SimpleCache implementation wasn't found."));
+            }
 
-			var url  = "http://blog.xamarin.com/feed/";
+            var keyValue = cacheService.Get<List<string>>(DemoKey);
+            if (keyValue != null)
+            {
+                CacheInfo = "key found on cache";
+                Items = new ObservableCollection<string>(keyValue);
+            }
+            else
+            {
+                CacheInfo = "key wasn't found on cache, you can save  it now";
+                Items = new ObservableCollection<string> { "Bananas", "Oranges", "Apples" };
+            }
+        }
 
-			var keyValue = _cacheService.Get<List<string>>(_demoKey);
-			if (keyValue != null) {
-				CacheInfo = "key found on cache";
-				Items = new ObservableCollection<string> (keyValue);
-			} else {
-				CacheInfo = "key wasn't found on cache, you can save  it now";
-				Items = new ObservableCollection<string>{ "Bananas", "Oranges", "Apples" };
-			}
-		}
+        /// <summary>
+        /// Backing field for the CacheInfo property.
+        /// </summary>
+        private string cacheInfo;
+        
+        /// <summary>
+        /// Gets or sets a <see cref="string"/> with cache information.
+        /// </summary>
+        /// <value>The cache information.</value>
+        public string CacheInfo
+        {
+            get
+            {
+                return cacheInfo;
+            }
+            set
+            {
+                this.ChangeAndNotify(ref cacheInfo, value);
+            }
+        }
 
+        /// <summary>
+        /// Backing field for the Items property.
+        /// </summary>
+        private ObservableCollection<string> items;
+        
+        /// <summary>
+        /// Gets or sets a collection of items in the cache.
+        /// </summary>
+        /// <value>The items in the cache.</value>
+        public ObservableCollection<string> Items
+        {
+            get
+            {
+                return items;
+            }
+            set
+            {
+                this.ChangeAndNotify(ref items, value);
+            }
+        }
 
-		private string _cacheInfo;
-		public string CacheInfo
-		{
-			get
-			{
-				return _cacheInfo;
-			}
-			set
-			{
-				this.ChangeAndNotify(ref _cacheInfo, value);
-			}
-		}
-		private ObservableCollection<string> _items;
-		public ObservableCollection<string> Items
-		{
-			get
-			{
-				return _items;
-			}
-			set
-			{
-				this.ChangeAndNotify(ref _items, value);
-			}
-		}
+        /// <summary>
+        /// Backing field to save items to the cache.
+        /// </summary>
+        private Command saveItemsToCacheCommand;
+        
+        /// <summary>
+        /// Gets the command to save items to the cache.
+        /// </summary>
+        /// <value>A <see cref="Command"/> to save items to the cache.</value>
+        public Command SaveItemsToCacheCacheCommand
+        {
+            get
+            {
+                return saveItemsToCacheCommand ?? (saveItemsToCacheCommand = new Command(
+                    () =>
+                    {
+                        cacheService.Remove(DemoKey);
+                        cacheService.Add(DemoKey, Items.ToList());
+                        CacheInfo = "key was saved on cache";
+                    },
+                    () => true));
+            }
+        }
 
-		private Command _saveItemsToCacheCommand;
-        public Command SaveItemsToCacheCacheCommand 
-		{
-			get
-			{ 
-				return _saveItemsToCacheCommand ?? (_saveItemsToCacheCommand = new Command (
-					() => {
-						_cacheService.Remove(_demoKey);
-						_cacheService.Add(_demoKey,Items.ToList());
-						CacheInfo = "key was saved on cache";
-					},
-					() => true)); 
-			}
-		}
+        /// <summary>
+        /// Backing field to clear items from the cache.
+        /// </summary>
+        private Command clearCacheCommand;
 
-		private Command _clearCacheCommand;
-		public Command ClearCacheCommand 
-		{
-			get
-			{ 
-				return _clearCacheCommand ?? (_clearCacheCommand = new Command (
-					 () => _cacheService.FlushAll(),
-					() => true)); 
-			}
-		}
-
-	}
+        /// <summary>
+        /// Gets the command to clear items from the cache.
+        /// </summary>
+        /// <value>A <see cref="Command"/> to clear items from the cache.</value>
+        public Command ClearCacheCommand
+        {
+            get
+            {
+                return clearCacheCommand ?? (clearCacheCommand = new Command(
+                     () => cacheService.FlushAll(),
+                    () => true));
+            }
+        }
+    }
 }
-
