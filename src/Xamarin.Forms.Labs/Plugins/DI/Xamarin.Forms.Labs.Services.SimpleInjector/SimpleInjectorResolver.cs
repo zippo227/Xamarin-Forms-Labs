630fea3f7dd53255cc0b6fe549ee486d7b7a0a2a
Sami M. Kallio <code.kallio@gmail.com>
2014-07-10 19:06:52 -0400

    Bug fix to SimpleInjector IResolver implementation
    https://github.com/XForms/Xamarin-Forms-Labs/issues/124#issuecomment-48674544

--- src/Xamarin.Forms.Labs/Plugins/DI/Xamarin.Forms.Labs.Services.SimpleInjector/SimpleInjectorResolver.cs
+++ src/Xamarin.Forms.Labs/Plugins/DI/Xamarin.Forms.Labs.Services.SimpleInjector/SimpleInjectorResolver.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SimpleInjector;
 
 namespace Xamarin.Forms.Labs.Services.SimpleContainer
 {
+    /// <summary>
+    /// The simple injector resolver.
+    /// </summary>
     public class SimpleInjectorResolver : IResolver
     {
         private readonly Container container;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleInjectorResolver"/> class.
+        /// </summary>
+        /// <param name="container">
+        /// The container.
+        /// </param>
         public SimpleInjectorResolver(Container container)
         {
             this.container = container;
@@ -18,24 +25,74 @@ namespace Xamarin.Forms.Labs.Services.SimpleContainer
 
         #region IResolver Members
 
+        /// <summary>
+        /// Resolve a dependency.
+        /// </summary>
+        /// <typeparam name="T">Type of instance to get.</typeparam>
+        /// <returns>An instance of {T} if successful, otherwise null.</returns>
         public T Resolve<T>() where T : class
         {
-            return this.container.GetInstance<T>();
+            try
+            {
+                return this.container.GetInstance<T>();
+            }
+            catch
+            {
+
+                return null;
+            }
         }
 
+        /// <summary>
+        /// Resolve a dependency by type.
+        /// </summary>
+        /// <param name="type">Type of object.</param>
+        /// <returns>An instance to type if found as <see cref="object"/>, otherwise null.</returns>
         public object Resolve(Type type)
         {
-            return this.container.GetInstance(type);
+            try
+            {
+                return this.container.GetInstance(type);
+            }
+            catch
+            {
+
+                return null;
+            }
         }
 
+        /// <summary>
+        /// Resolve a dependency.
+        /// </summary>
+        /// <typeparam name="T">Type of instance to get.</typeparam>
+        /// <returns>All instances of {T} if successful, otherwise null.</returns>
         public IEnumerable<T> ResolveAll<T>() where T : class
         {
-            return this.container.GetAllInstances<T>();
+            try
+            {
+                return this.container.GetAllInstances<T>();
+            }
+            catch
+            {
+                return Enumerable.Empty<T>();
+            }
         }
 
+        /// <summary>
+        /// Resolve a dependency by type.
+        /// </summary>
+        /// <param name="type">Type of object.</param>
+        /// <returns>All instances of type if found as <see cref="object"/>, otherwise null.</returns>
         public IEnumerable<object> ResolveAll(Type type)
         {
-            return this.container.GetAllInstances(type);
+            try
+            {
+                return this.container.GetAllInstances(type);
+            }
+            catch
+            {
+                return Enumerable.Empty<object>();
+            }
         }
 
         #endregion
