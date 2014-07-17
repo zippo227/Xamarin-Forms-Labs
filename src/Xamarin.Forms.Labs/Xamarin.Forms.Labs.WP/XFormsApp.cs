854d5f69416dbdaf3b505ef31657447dbc3950af
Sami M. Kallio <code.kallio@gmail.com>
2014-07-17 18:28:02 -0400

    Added device orientation enum

--- src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.WP/XFormsApp.cs
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.WP/XFormsApp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,27 @@ using Xamarin.Forms.Labs.Mvvm;
 
 namespace Xamarin.Forms.Labs.WP8
 {
+    /// <summary>
+    /// The Xamarin Forms Labs Windows Phone Application.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
     public class XFormsAppWP : XFormsApp<Application>
     {
+        /// <summary>
+        /// Initializes the specified context.
+        /// </summary>
+        /// <param name="app">The native application.</param>
         protected override void OnInit(Application app)
         {
-            this.AppContext.Startup += (o, e) => { this.OnStartup(); };
-            this.AppContext.Exit += (o, e) => { this.OnClosing(); };
-            this.AppContext.UnhandledException += (o, e) => { this.OnError(e.ExceptionObject); };
+            this.AppContext.Startup += (o, e) => this.OnStartup();
+            this.AppContext.Exit += (o, e) => this.OnClosing();
+            this.AppContext.UnhandledException += (o, e) => this.OnError(e.ExceptionObject);
             this.AppDataDirectory = ApplicationData.Current.LocalFolder.Path;
 
-            foreach (var a in app.ApplicationLifetimeObjects)
+            foreach (var svc in app.ApplicationLifetimeObjects.OfType<PhoneApplicationService>())
             {
-                var svc = a as PhoneApplicationService;
-                if (svc != null)
-                {
-                    svc.Activated += (o, e) => { this.OnResumed(); };
-                    svc.Deactivated += (o, e) => { this.OnSuspended(); };
-                }
+                svc.Activated += (o, e) => this.OnResumed();
+                svc.Deactivated += (o, e) => this.OnSuspended();
             }
 
             base.OnInit(app);
