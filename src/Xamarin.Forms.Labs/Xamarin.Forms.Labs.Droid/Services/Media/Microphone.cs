467f0d181f7f503d68d6f171868af18c7bc1cb7a
Sami M. Kallio <code.kallio@gmail.com>
2014-08-29 09:06:38 -0400

    Initial draft of IAudioStream
    Audio streams for iOS, Android & WP8. WaveRecorder class to store WAVE
    audio files.

---
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.Droid/Services/Media/Microphone.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Android.Media;
+using Xamarin.Forms.Labs.Mvvm;
+using Xamarin.Forms.Labs.Services.Media;
+
+namespace Xamarin.Forms.Labs.Droid.Services.Media
+{
+    public class Microphone : IAudioStream
+    {
+        private readonly int bufferSize;
+
+        /// <summary>
+        /// The audio source.
+        /// </summary>
+        private readonly AudioRecord audioSource;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Xamarin.Forms.Labs.Droid.Services.Media.Microphone"/> class.
+        /// </summary>
+        /// <param name="sampleRate">Sample rate.</param>
+        /// <param name="bufferSize">Buffer size.</param>
+        public Microphone(int sampleRate, int bufferSize)
+        {
+            this.bufferSize = bufferSize;
+            this.audioSource = new AudioRecord(
+                AudioSource.Mic,
+                sampleRate,
+                ChannelIn.Mono,
+                Encoding.Pcm16bit,
+                this.bufferSize);
+
+            this.Start = new RelayCommand<int>(
+                this.StartRecording,
+                rate => this.SupportedSampleRates.Contains(rate) && this.audioSource != null && !this.Active
+                );
+
+            this.Stop = new Command(
+                () => this.audioSource.Stop(),
+                () => this.Active
+                );
+
+            this.SupportedSampleRates = (new[] { 8000, 11025, 16000, 22050, 44100 }).Where( 
+                rate => AudioRecord.GetMinBufferSize(rate, ChannelIn.Default, Encoding.Pcm16bit) > 0
+                ).ToList();
+        }
+
+        /// <summary>
+        /// Occurs when new audio has been streamed.
+        /// </summary>
+        public event EventHandler<EventArgs<byte[]>> OnBroadcast;
+
+        /// <summary>
+        /// Gets the sample rate.
+        /// </summary>
+        /// <value>
+        /// The sample rate.
+        /// </value>
+        public int SampleRate
+        {
+            get
+            {
+                return this.audioSource.SampleRate;
+            }
+        }
+
+        /// <summary>
+        /// Gets bits per sample.
+        /// </summary>
+        public int BitsPerSample
+        {
+            get
+            {
+                return (this.audioSource.AudioFormat == Encoding.Pcm16bit) ? 16 : 8;
+            }
+        }
+
+        /// <summary>
+        /// Gets the channel count.
+        /// </summary>
+        /// <value>
+        /// The channel count.
+        /// </value>        
+        public int ChannelCount
+        {
+            get
+            {
+                return this.audioSource.ChannelCount;
+            }
+        }
+
+        /// <summary>
+        /// Gets the average data transfer rate
+        /// </summary>
+        /// <value>The average data transfer rate in bytes per second.</value>
+        public int AverageBytesPerSecond
+        {
+            get
+            {
+                return this.SampleRate * this.BitsPerSample / 8 * this.ChannelCount;
+            }
+        }
+
+        public bool Active
+        {
+            get
+            {
+                return (this.audioSource.RecordingState == RecordState.Recording);
+            }
+        }
+
+        public IEnumerable<int> SupportedSampleRates
+        {
+            get;
+            private set;
+        }
+
+        public ICommand Start
+        {
+            get;
+            private set;
+        }
+
+        public ICommand Stop
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Start recording from the hardware audio source.
+        /// </summary>
+        private void StartRecording(int rate)
+        {
+            this.audioSource.StartRecording();
+
+            Task.Run(async () => 
+            {
+                do
+                {
+                    await Record();
+                } 
+                while (this.Active) ;
+            }
+            );
+        }
+
+        /// <summary>
+        /// Record from the microphone and broadcast the buffer.
+        /// </summary>
+        private async Task Record()
+        {
+            var buffer = new byte[this.bufferSize];
+
+            var readCount = await this.audioSource.ReadAsync(buffer, 0, this.bufferSize);
+            
+            this.OnBroadcast.Invoke<byte[]>(this, buffer);
+        }
+    }
+}
---
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs/Services/Media/WaveRecorder.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.Labs.Services.Media
+{
+    public class WaveRecorder
+    {
+        private StreamWriter streamWriter;
+        private BinaryWriter writer;
+        private int byteCount;
+        private IAudioStream stream;
+
+        ~WaveRecorder()
+        {
+            StopRecorder();
+        }
+
+        public bool StartRecorder(IAudioStream stream, string fileName)
+        {
+            if (this.stream != null || stream == null)
+            {
+                return false;
+            }
+
+            this.stream = stream;
+
+            try
+            {
+                //this.streamWriter = new StreamWriter(fileName, false);
+                this.writer = new BinaryWriter(this.streamWriter.BaseStream, Encoding.UTF8);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            this.byteCount = 0;
+            this.stream.OnBroadcast += OnStreamBroadcast;
+
+            if (this.stream.Start.CanExecute(this))
+            {
+                this.stream.Start.Execute(this);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void StopRecorder()
+        {
+            if (this.stream != null)
+            {
+                this.stream.OnBroadcast -= OnStreamBroadcast;
+                if (this.stream.Stop.CanExecute(this))
+                {
+                    this.stream.Stop.Execute(this);
+                }
+            }
+
+            if (this.streamWriter != null && this.streamWriter.BaseStream.CanWrite)
+            {
+                this.WriteHeader();
+                this.streamWriter.Dispose();
+                this.streamWriter = null;
+            }
+
+            this.stream = null;
+        }
+
+        private void OnStreamBroadcast(object sender, EventArgs<byte[]> eventArgs)
+        {
+            this.writer.Write(eventArgs.Value);
+            this.byteCount += eventArgs.Value.Length;
+        }
+
+
+        private void WriteHeader()
+        {
+            this.writer.Seek(0, SeekOrigin.Begin);
+            // chunk ID
+            this.writer.Write('R');
+            this.writer.Write('I');
+            this.writer.Write('F');
+            this.writer.Write('F');
+
+            this.writer.Write(this.byteCount + 36);
+            this.writer.Write('W');
+            this.writer.Write('A');
+            this.writer.Write('V');
+            this.writer.Write('E');
+
+            this.writer.Write('f');
+            this.writer.Write('m');
+            this.writer.Write('t');
+            this.writer.Write(' ');
+
+            this.writer.Write(16);
+            this.writer.Write((short)1);
+
+            this.writer.Write((short)this.stream.ChannelCount);
+            this.writer.Write(this.stream.SampleRate);
+            this.writer.Write(this.stream.SampleRate * 2);
+            this.writer.Write((short)2);
+            this.writer.Write((short)this.stream.BitsPerSample);
+            this.writer.Write('d');
+            this.writer.Write('a');
+            this.writer.Write('t');
+            this.writer.Write('a');
+            this.writer.Write(this.byteCount);
+        }
+    }
+}
