9a78fc0d89c09892a523282b31373dc9c5d15b94
Sami M. Kallio <code.kallio@gmail.com>
2014-09-16 15:46:04 -0400

    Added Separator renderer for WP8
    No orientation support yet. Renders the sample page.

--- src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.iOS/Controls/Separator/SeparatorRenderer.cs
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.iOS/Controls/Separator/SeparatorRenderer.cs
@@ -6,43 +6,45 @@ using Xamarin.Forms.Platform.iOS;
 using System.ComponentModel;
 
 [assembly: ExportRenderer(typeof(Separator), typeof(SeparatorRenderer))]
+
 namespace Xamarin.Forms.Labs.iOS
 {
-	public class SeparatorRenderer : ViewRenderer<Separator,UISeparator>
-	{
-		public SeparatorRenderer()
-		{
-		}
+    public class SeparatorRenderer : ViewRenderer<Separator,UISeparator>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Separator> e)
+        {
+            base.OnElementChanged(e);
 
-		protected override void OnElementChanged(ElementChangedEventArgs<Separator> e)
-		{
-			base.OnElementChanged(e);
-			var separator = new UISeparator(this.Bounds);
-			this.BackgroundColor = Color.Transparent.ToUIColor();
-			this.SetNativeControl(separator);
-			this.SetProperties ();
-			this.Element.PropertyChanged += this.OnPropertyChanged;
+            if (e.NewElement == null)
+            {
+                return;
+            }
 
+            if (this.Control == null)
+            {
+                this.BackgroundColor = Color.Transparent.ToUIColor();
+                this.SetNativeControl(new UISeparator(this.Bounds));
+            }
 
-		}
+            this.SetProperties ();
+        }
 
-	
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            this.SetProperties();
+        }
 
-		private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			this.SetProperties ();
-		}
-
-		private void SetProperties()
-		{
-			var separator = Control;
-			separator.Thickness = Element.Thickness;
-			separator.StrokeColor = Element.Color.ToUIColor();
-			separator.StrokeType = Element.StrokeType;
-			separator.Orientation = Element.Orientation;
-			separator.SpacingBefore = Element.SpacingBefore;
-			separator.SpacingAfter = Element.SpacingAfter;
-		}
-	}
+        private void SetProperties()
+        {
+            var separator = Control;
+            separator.Thickness = Element.Thickness;
+            separator.StrokeColor = Element.Color.ToUIColor();
+            separator.StrokeType = Element.StrokeType;
+            separator.Orientation = Element.Orientation;
+            separator.SpacingBefore = Element.SpacingBefore;
+            separator.SpacingAfter = Element.SpacingAfter;
+        }
+    }
 }
 
