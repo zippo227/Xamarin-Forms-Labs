0683f4808544d813d59c29920435d9312db25115
Rui Marinho <me@ruimarinho.net>
2014-09-23 21:19:28 +0100

    update resources android sample

--- samples/Xamarin.Forms.Labs.Sample.Droid/Resources/Resource.designer.cs
+++ samples/Xamarin.Forms.Labs.Sample.Droid/Resources/Resource.designer.cs
@@ -162,37 +162,40 @@ namespace Xamarin.Forms.Labs.Sample.Droid
 			public const int ad16 = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int calendar_bg_selector = 2130837505;
+			public const int appicon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int Icon = 2130837506;
+			public const int calendar_bg_selector = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int icon_facebook = 2130837507;
+			public const int Icon = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int icon_google = 2130837508;
+			public const int icon_facebook = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int icon_microsoft = 2130837509;
+			public const int icon_google = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int icon_twitter = 2130837510;
+			public const int icon_microsoft = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int imgNotAvailable = 2130837511;
+			public const int icon_twitter = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int pie27_32 = 2130837512;
+			public const int imgNotAvailable = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int pie30_32 = 2130837513;
+			public const int pie27_32 = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int services1_32 = 2130837514;
+			public const int pie30_32 = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int settings20_32 = 2130837515;
+			public const int services1_32 = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int settings20_32 = 2130837516;
 			
 			static Drawable()
 			{
