9f32d3f22b92386e4b60c86bc7ee41c134387128
mitchmilam <mitch@infinite-x.net>
2014-11-06 22:43:09 -0600

    Updated main App.cs to make it easier to add new pages. Corrected the the names of some of the pages.

--- samples/Xamarin.Forms.Labs.Sample/App.cs
+++ samples/Xamarin.Forms.Labs.Sample/App.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Labs.Controls;
 using Xamarin.Forms.Labs.Mvvm;
 using Xamarin.Forms.Labs.Sample.Pages.Controls;
 using Xamarin.Forms.Labs.Sample.Pages.Controls.Charts;
+using Xamarin.Forms.Labs.Sample.Pages.Controls.DynamicList;
 using Xamarin.Forms.Labs.Sample.Pages.Services;
 using Xamarin.Forms.Labs.Services;
 using XLabs.Ioc;
@@ -89,29 +95,27 @@ namespace Xamarin.Forms.Labs.Sample
         /// <returns>Content Page.</returns>
         private static ContentPage GetServicesPage(VisualElement mainPage)
         {
-            var services = new ContentPage
+            var listItems = new List<string>()
             {
-                Title = "Services",
-                Icon = Device.OnPlatform("services1_32.png", "services1_32.png", "Images/services1_32.png"),
+                "TextToSpeech",
+                "DeviceExtended",
+                "PhoneService",
+                "GeoLocator",
+                "Camera",
+                "Accelerometer",
+                "Display",
+                "Cache",
+                "Sound",
+                "FontManager",
+                "NFC",
+                "Email",
+                //"WaveRecorder",
+                //"Bluetooth",
             };
+
             var lstServices = new ListView
             {
-                ItemsSource = new List<string>() {
-                    "TextToSpeech",
-                    "DeviceExtended",
-                    "PhoneService",
-                    "GeoLocator",
-                    "Camera",
-                    "Accelerometer",
-                    "Display",
-                    "Cache",
-                    "Sound",
-                    //"Bluetooth",
-                    "FontManager",
-                    "NFC",
-                    //"WaveRecorder",
-                    "Email"
-                }
+                ItemsSource = listItems
             };
 
             lstServices.ItemSelected += async (sender, e) =>
@@ -162,7 +166,14 @@ namespace Xamarin.Forms.Labs.Sample
                         break;
                 }
             };
-            services.Content = lstServices;
+
+            var services = new ContentPage
+            {
+                Title = "Services",
+                Icon = Device.OnPlatform("services1_32.png", "services1_32.png", "Images/services1_32.png"),
+                Content = lstServices,
+            };
+
             return services;
         }
 
@@ -173,124 +184,45 @@ namespace Xamarin.Forms.Labs.Sample
         /// <returns>Content Page.</returns>
         private static ContentPage GetControlsPage(VisualElement mainPage)
         {
-            var controls = new ContentPage
+            var listItems = new SortedDictionary<string, Type>
             {
-                Title = "Controls",
-                Icon = Device.OnPlatform("settings20_32.png", "settings20.png", "Images/settings20.png"),
-            };
-
-            var listItems = new List<string>
-            {
-                "Autocomplete",
-                "ButtonGroup",
-                "Calendar",
-                "CameraView",
-                "CheckBox",
-                "CircleImage",
-                "DynamicListView",
-                "ExtendedCell",
-                "ExtendedEntry",
-                "ExtendedLabel",
-                "ExtendedScrollView",
-                "ExtendedSlider",
-                "GridView",
-                "HybridWebView",
-                "ImageButton",
-                "ImageGallery",
-                "Popup",
-                "RepeaterView",
-                "Segment",
-                "Separator",
-                "WebImage",
+                {"Autocomplete",  typeof(AutoCompletePage)},
+                {"ButtonGroup", typeof(ButtonGroupPage)},
+                {"Calendar", typeof(CalendarPage)},
+                {"CameraView", typeof(CameraViewPage)},
+                {"CheckBox", typeof(CheckBoxPage)},
+                {"CircleImage", typeof(CircleImagePage)},
+                {"DynamicListView", typeof(DynamicListView)},
+                {"ExtendedCell", typeof(ExtendedCellPage)},
+                {"ExtendedEntry", typeof(ExtendedEntryPage)},
+                {"ExtendedLabel", typeof(ExtendedLabelPage)},
+                {"ExtendedScrollView", typeof(ExtendedScrollViewPage)},
+                {"ExtendedSlider", typeof(ExtendedSliderPage)},
+                {"GridView", typeof(GridViewPage)},
+                {"HybridWebView", typeof(CanvasWebHybrid)},
+                {"ImageButton", typeof(ButtonPage)},
+                {"ImageGallery", typeof(ImageGalleryPage)},
+                {"Popup", typeof(PopupPage)},
+                {"RepeaterView", typeof(RepeaterViewPage)},
+                {"Segment", typeof(SegmentPage)},
+                {"Separator", typeof(SeparatorPage)},
+                {"WebImage", typeof(WebImagePage)},
             };
 
             // This is actually a lot of work just to enable something
             // for iOS only, but oh well.
             if (Device.OS == TargetPlatform.iOS)
             {
-                var index = listItems.IndexOf("Segment");
-
-                listItems.Insert(index + 1, "SegmentedControlView");
+                listItems.Add("SegmentedControlView", typeof(SegmentedControlViewPage));
             }
 
-            var lstControls = new ListView
+            var controls = new ContentPage
             {
-                ItemsSource = listItems
+                Title = "Controls",
+                Icon = Device.OnPlatform("settings20_32.png", "settings20.png", "Images/settings20.png"),
+                Content = BuildListView(mainPage, listItems),
             };
 
-            lstControls.ItemSelected += async (sender, e) =>
-            {
-                switch (e.SelectedItem.ToString().ToLower())
-                {
-                    case "autocomplete":
-                        await mainPage.Navigation.PushAsync(new AutoCompletePage());
-                        break;
-                    case "imagebutton":
-                        await mainPage.Navigation.PushAsync(new ButtonPage());
-                        break;
-                    case "calendar":
-                        await mainPage.Navigation.PushAsync(new CalendarPage());
-                        break;
-                    case "cameraview":
-                        await mainPage.Navigation.PushAsync(new CameraViewPage());
-                        break;
-                    case "checkbox":
-                        await mainPage.Navigation.PushAsync(new CheckBoxPage());
-                        break;
-                    case "dynamiclistview":
-                        await mainPage.Navigation.PushAsync(new Xamarin.Forms.Labs.Sample.Pages.Controls.DynamicList.DynamicListView());
-                        break;
-                    case "extendedcell":
-                        await mainPage.Navigation.PushAsync(new ExtendedCellPage());
-                        break;
-                    case "extendedentry":
-                        await mainPage.Navigation.PushAsync(new ExtendedEntryPage());
-                        break;
-                    case "extendedlabel":
-                        await mainPage.Navigation.PushAsync(new ExtendedLabelPage());
-                        break;
-                    case "extendedscrollview":
-                        await mainPage.Navigation.PushAsync(new Pages.Controls.ExtendedScrollView());
-                        break;
-                    case "extendedslider":
-                        await mainPage.Navigation.PushAsync(new ExtendedSliderPage());
-                        break;
-                    case "gridview":
-                        await mainPage.Navigation.PushAsync(new GridViewPage());
-                        break;
-                    case "hybridwebview":
-                        await mainPage.Navigation.PushAsync(new CanvasWebHybrid());
-                        break;
-                    case "imagegallery":
-                        await mainPage.Navigation.PushAsync(new Pages.Controls.ImageGallery());
-                        break;
-                    case "popup":
-                        await mainPage.Navigation.PushAsync(new PopupPage());
-                        break;
-                    case "repeaterview":
-                        await mainPage.Navigation.PushAsync(new RepeaterViewPage());
-                        break;
-                    case "segment":
-                        await mainPage.Navigation.PushAsync(new SegmentPage());
-                        break;
-                    case "segmentedcontrolview":
-                        await mainPage.Navigation.PushAsync(new SegmentedControlViewPage());
-                        break;
-                    case "separator":
-                        await mainPage.Navigation.PushAsync(new SeparatorPage());
-                        break;
-                    case "webimage":
-                        await mainPage.Navigation.PushAsync(new WebImagePage());
-                        break;
-                    case "buttongroup":
-                        await mainPage.Navigation.PushAsync(new ButtonGroupPage());
-                        break;
-                    case "circleimage":
-                        await mainPage.Navigation.PushAsync(new CircleImagePage());
-                        break;
-                }
-            };
-            controls.Content = lstControls;
             return controls;
         }
 
@@ -301,46 +233,89 @@ namespace Xamarin.Forms.Labs.Sample
         /// <returns>Content Page.</returns>
         private static ContentPage GetChartingPage(VisualElement mainPage)
         {
+            var listItems = new SortedDictionary<string, Type>
+            {
+                 {"Bar", typeof(BarChartPage)},
+                 {"Line", typeof(LineChartPage)},
+                 {"Combination", typeof(CombinationPage)},
+                 {"Pie", typeof(PieChartPage)},
+                 {"Databound combination", typeof(BoundChartPage)},
+            };
+
             var controls = new ContentPage
             {
                 Title = "Charts",
                 Icon = Device.OnPlatform("pie30_32.png", "pie30_32.png", "Images/pie30_32.png"),
+                Content = BuildListView(mainPage, listItems),
             };
-            var lstControls = new ListView
-            {
-                ItemsSource = new List<string>() {
-                    "Bar",
-                    "Line",
-                    "Combination",
-                    "Pie",
-                    "Databound combination"
-                }
-            };
-            lstControls.ItemSelected += async (sender, e) =>
-            {
-                switch (e.SelectedItem.ToString().ToLower())
-                {
-                    case "bar":
-                        await mainPage.Navigation.PushAsync(new BarChartPage());
-                        break;
-                    case "line":
-                        await mainPage.Navigation.PushAsync(new LineChartPage());
-                        break;
-                    case "combination":
-                        await mainPage.Navigation.PushAsync(new CombinationPage());
-                        break;
-                    case "pie":
-                        await mainPage.Navigation.PushAsync(new PieChartPage());
-                        break;
-                    case "databound combination":
-                        await mainPage.Navigation.PushAsync(new BoundChartPage());
-                        break;
-                    default:
-                        break;
-                }
-            };
-            controls.Content = lstControls;
+
             return controls;
+        }
+
+        /// <summary>
+        /// Build a ListView associated with a SortedDictionary as the DataSource
+        /// </summary>
+        /// <param name="mainPage">Parent page for the page containing the list view</param>
+        /// <param name="listItems">List of items to display</param>
+        /// <returns></returns>
+        private static ListView BuildListView(VisualElement mainPage, SortedDictionary<string, Type> listItems)
+        {
+            var listView = new ListView
+            {
+                ItemsSource = listItems,
+                ItemTemplate = new DataTemplate(typeof(TextCell)),
+            };
+
+            listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Key");
+
+            listView.ItemSelected += async (sender, e) =>
+            {
+                Type result = null;
+
+                // This is actually some type of bug with Xamarin.
+                // On iOS the SortedDiectionary entries are DictionaryEntries
+                // on WP, they are KeyValuePairs.
+                // Using the wrong type causes a casting exception.
+                switch (Device.OS)
+                {
+                    case TargetPlatform.Android:
+                    case TargetPlatform.iOS:
+                        var item = (DictionaryEntry) e.SelectedItem;
+                        result = (Type)item.Value;
+                        break;
+                    case TargetPlatform.WinPhone:
+                        result = ((KeyValuePair<string, Type>)e.SelectedItem).Value;
+                        break;
+                }
+
+                await ShowPage(mainPage, result);
+            };
+
+            return listView;
+        }
+
+        /// <summary>
+        /// Shows a page asynchronously by locating the default constructor, creating the page,
+        /// the pushing it onto the navigation stack.
+        /// </summary>
+        /// <param name="parentPage">Parent Page</param>
+        /// <param name="pageType">Type of page to show</param>
+        /// <returns></returns>
+        private static async Task ShowPage(VisualElement parentPage, Type pageType)
+        {
+            // Get all the constructors of the page type.
+            var constructors = pageType.GetTypeInfo().DeclaredConstructors;
+
+            foreach (
+                var page in
+                    from constructor in constructors
+                    where constructor.GetParameters().Length == 0
+                    select (Page)constructor.Invoke(null))
+            {
+                await parentPage.Navigation.PushAsync(page);
+
+                break;
+            }
         }
     }
 }
--- samples/Xamarin.Forms.Labs.Sample/Pages/Controls/ExtendedScrollView.xaml.cs
+++ samples/Xamarin.Forms.Labs.Sample/Pages/Controls/ExtendedScrollView.xaml.cs
@@ -1,43 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using Xamarin.Forms;
 
 namespace Xamarin.Forms.Labs.Sample.Pages.Controls
-{    
-    public partial class ExtendedScrollView : ContentPage
-    {    
+{
+    public partial class ExtendedScrollViewPage : ContentPage
+    {
         int _imageHeight = 200;
         bool _displayAlert = false;
-        public ExtendedScrollView ()
+        public ExtendedScrollViewPage()
         {
-            InitializeComponent ();
+            InitializeComponent();
 
-            var container = new StackLayout () { 
-                HorizontalOptions= LayoutOptions.FillAndExpand,
-                VerticalOptions = LayoutOptions.FillAndExpand };
-            var buttonToScroll = new Button () { Text = "Scroll to end" };
-            container.Children.Add (buttonToScroll);
-            for (int i = 0; i < 10; i++) {
-                container.Children.Add (new Image { 
+            var container = new StackLayout()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var buttonToScroll = new Button() { Text = "Scroll to end" };
+
+            container.Children.Add(buttonToScroll);
+
+            for (var i = 0; i < 10; i++)
+            {
+                container.Children.Add(new Image
+                {
                     Aspect = Aspect.AspectFill,
-                    Source = ImageSource.FromUri (new Uri("http://www.stockvault.net/data/2011/05/31/124348/small.jpg")),
+                    Source = ImageSource.FromUri(new Uri("http://www.stockvault.net/data/2011/05/31/124348/small.jpg")),
                     HeightRequest = _imageHeight
 
                 });
             }
-            var sv = new Xamarin.Forms.Labs.Controls.ExtendedScrollView { Content = container, HorizontalOptions= LayoutOptions.FillAndExpand,
-                VerticalOptions = LayoutOptions.FillAndExpand};
-            sv.Scrolled+= async (ScrollView arg1, Rectangle arg2) => {
-                if(arg2.Y > sv.Bounds.Height && !_displayAlert)
-                {  
-                    _displayAlert = true;
-                   await DisplayAlert("Scroll event", "User scrolled pass bounds","Ok","cancel");
-                 
+            var sv = new Labs.Controls.ExtendedScrollView
+            {
+                Content = container,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            sv.Scrolled += async (arg1, arg2) =>
+            {
+                if (!(arg2.Y > sv.Bounds.Height) || _displayAlert)
+                {
+                    return;
                 }
+
+                _displayAlert = true;
+                await DisplayAlert("Scroll event", "User scrolled pass bounds", "Ok", "cancel");
             };
-            buttonToScroll.Clicked+= (object sender, EventArgs e) => {
-                sv.Position = new Point(sv.Position.X, sv.ContentSize.Height-_imageHeight);
+
+            buttonToScroll.Clicked += (sender, e) =>
+            {
+                sv.Position = new Point(sv.Position.X, sv.ContentSize.Height - _imageHeight);
             };
+
             Content = sv;
         }
 
