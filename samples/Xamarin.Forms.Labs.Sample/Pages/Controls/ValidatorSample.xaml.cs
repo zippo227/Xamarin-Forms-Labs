af63da3b0474f614529796f20b6f5fcb3935b2b0
Choran66 <charles.horan@gmail.com>
2014-11-08 07:35:23 -0500

    Validator sample page
    Added triggers to ValidatorActions, the sample shows how to use them.
    :dart:

---
+++ samples/Xamarin.Forms.Labs.Sample/Pages/Controls/ValidatorSample.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Xamarin.Forms.Labs.Sample.Pages.Controls
+{
+    public partial class ValidatorSample : ContentPage
+    {
+        public ValidatorSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
