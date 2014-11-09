48c33e359b6ff05c5ff4888cf587d3be8d893b09
Choran66 <charles.horan@gmail.com>
2014-11-08 20:15:47 -0500

    Refactoring ++
    Refactored the Validation code into the Validation Namespace
    Added PropertySetters to Action
    Added the ability for the user to supply custom validators at runtime
    Renamed classes to make Xaml cleaner
    validation sample showing all the validators
    
    Next up...validator documentation.   I think I should tackle that doc
    first since this is the most complex component I've put in thus far.

---
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs/Validation/PropertySetters.cs
@@ -0,0 +1,10 @@
+﻿namespace Xamarin.Forms.Labs.Validation
+{
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// Collection of <see cref="PropertySetter"/>
+    /// </summary>
+    /// Element created at 08/11/2014,3:58 PM by Charles
+    public class PropertySetters : ObservableCollection<PropertySetter>{}
+}
