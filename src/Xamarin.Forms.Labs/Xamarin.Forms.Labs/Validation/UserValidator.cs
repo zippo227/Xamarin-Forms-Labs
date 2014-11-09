298b2a3af4b20cb49d8604905c2f03e04e62238f
Choran66 <charles.horan@gmail.com>
2014-11-08 20:15:47 -0500

    Refactoring ++
    Refactored the Validation code into the Validation Namespace
    Added PropertySetters to Action
    Added the ability for the user to supply custom validators at runtime
    Renamed classes to make Xaml cleaner
    validation sample showing all the validators
    
    Next up...validator documentation.   I think I should tackle that doc
    first since this is the most complex component I've put in thus far.

---
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs/Validation/UserValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Xamarin.Forms.Labs.Validation
+{
+    using System;
+
+    internal class UserValidator : ValidatorPredicate
+    {
+        public UserValidator(Validators id, PredicatePriority priority, Func<Rule, string, bool> eval) : base(id, priority, eval) {
+        }
+
+        public string UserName { get; set; }
+    }
+}
