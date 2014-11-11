9fb2fbcbf55c199e6dcad773ea8f8a64d32a6675
Choran66 <charles.horan@gmail.com>
2014-11-10 23:58:54 -0500

    Revert "Restoring RepeaterView"
    This reverts commit 75df35c16c0646144aa3ec6a838e2b2b8b963602.

--- src/Xamarin.Forms.Labs/Xamarin.Forms.Labs/Controls/RepeaterView.cs
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs/Controls/RepeaterView.cs
@@ -1,109 +1,83 @@
-﻿namespace Xamarin.Forms.Labs.Controls
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Xamarin.Forms.Labs.Controls
 {
-    using System;
-    using System.Collections.ObjectModel;
-    using System.Collections.Specialized;
-    using System.Windows.Input;
+    using System.Collections.Generic;
     using Xamarin.Forms.Labs.Exceptions;
 
-    /// <summary>
-    /// Simple list control to dispaly an ObservableCollection(T)
-    /// </summary>
-    /// <typeparam name="T">The type contained in teh collection to dispaly</typeparam>
-    /// Element created at 10/11/2014,10:54 PM by Charles
     public class RepeaterView<T> : StackLayout
     {
+        private class CollectionChangedHandle : IDisposable
+        {
+            private INotifyCollectionChanged _itemsSource;
+            private NotifyCollectionChangedEventHandler _eventHandler;
 
-        /// <summary>
-        /// Definition for <see cref="ItemTemplate"/>
-        /// </summary>
-        /// Element created at 10/11/2014,10:57 PM by Charles
+            public CollectionChangedHandle(RepeaterView<T> repeater, IEnumerable<T> itemsSource)
+            {
+                _itemsSource = itemsSource as INotifyCollectionChanged;
+
+                if (_itemsSource != null)
+                {
+                    _eventHandler = new NotifyCollectionChangedEventHandler(repeater.ItemsSource_CollectionChanged);
+                    _itemsSource.CollectionChanged += repeater.ItemsSource_CollectionChanged;
+                }
+            }
+
+            public void Dispose()
+            {
+                if (_eventHandler != null)
+                {
+                    _itemsSource.CollectionChanged -= _eventHandler;
+                    _eventHandler = null;
+                }
+            }
+        }
+
         public static readonly BindableProperty ItemTemplateProperty =
         BindableProperty.Create<RepeaterView<T>, DataTemplate>(p => p.ItemTemplate, default(DataTemplate));
 
-        /// <summary>
-        /// Definition for <see cref="ItemsSource"/>
-        /// </summary>
-        /// Element created at 10/11/2014,10:58 PM by Charles
         public static readonly BindableProperty ItemsSourceProperty =
-            BindableProperty.Create<RepeaterView<T>, ObservableCollection<T>>(p => p.ItemsSource, default(ObservableCollection<T>), BindingMode.OneWay, null, ItemsChanged);
+            BindableProperty.Create<RepeaterView<T>, IEnumerable<T>>(p => p.ItemsSource, Enumerable.Empty<T>(), BindingMode.OneWay, null, ItemsChanged);
 
-        /// <summary>
-        /// Definition for <see cref="ItemClickCommand"/>
-        /// </summary>
-        /// Element created at 10/11/2014,10:56 PM by Charles
         public static BindableProperty ItemClickCommandProperty =
             BindableProperty.Create<RepeaterView<T>, ICommand>(x => x.ItemClickCommand, null);
 
-        /// <summary>
-        /// Definition for <see cref="TemplateSelector"/>
-        /// </summary>
-        /// Element created at 10/11/2014,10:58 PM by Charles
         public static readonly BindableProperty TemplateSelectorProperty =
             BindableProperty.Create<RepeaterView<T>, TemplateSelector>(x => x.TemplateSelector, default(TemplateSelector));
 
-        /// <summary>
-        /// Definition of the ItemAdded EventHandler delegate
-        /// </summary>
-        /// <param name="sender">this control</param>
-        /// <param name="args">The <see cref="RepeaterViewItemAddedEventArgs"/> instance containing the event data.</param>
-        /// Element created at 10/11/2014,10:59 PM by Charles
         public delegate void RepeaterViewItemAddedEventHandler(object sender, RepeaterViewItemAddedEventArgs args);
 
-        /// <summary>Occurs when [item created].</summary>
-        /// Element created at 10/11/2014,10:59 PM by Charles
         public event RepeaterViewItemAddedEventHandler ItemCreated;
 
+        private IDisposable _collectionChangedHandle;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RepeaterView{T}"/> class.
-        /// </summary>
-        /// Element created at 10/11/2014,11:00 PM by Charles
         public RepeaterView()
         {
             Spacing = 0;
         }
 
-        /// <summary>
-        /// The items source property
-        /// Since Items source must implement INotifyCollectionChanged, it is reasonable
-        /// to use ObservableCollection(T) as the most base collection class
-        /// that is acceptable.
-        /// </summary>
-        /// Element created at 10/11/2014,10:55 PM by Charles
-        public ObservableCollection<T> ItemsSource
+        public IEnumerable<T> ItemsSource
         {
             get { return (ObservableCollection<T>)GetValue(ItemsSourceProperty); }
             set { SetValue(ItemsSourceProperty, value); }
         }
 
-        /// <summary>Gets or sets the template selector.</summary>
-        /// <value>The template selector picks a datatemplate based on the type of the item.</value>
-        /// Element created at 10/11/2014,11:00 PM by Charles
         public TemplateSelector TemplateSelector
         {
             get { return (TemplateSelector)GetValue(TemplateSelectorProperty); }
             set { SetValue(TemplateSelectorProperty, value); }
         }
 
-        /// <summary>Gets or sets the item click command.</summary>
-        /// <value>
-        /// The item click command.  This command is called when an item
-        /// has been tapped and the CanExecute callback is true
-        /// </value>
-        /// Element created at 10/11/2014,11:00 PM by Charles
         public ICommand ItemClickCommand
         {
             get { return (ICommand)this.GetValue(ItemClickCommandProperty); }
             set { SetValue(ItemClickCommandProperty, value); }
         }
 
-        
-        /// <summary>
-        /// The item template property, this can be used on it's own
-        /// or in conjunction with the template selector
-        /// </summary>
-        /// Element created at 10/11/2014,10:54 PM by Charles
         public DataTemplate ItemTemplate
         {
             get { return (DataTemplate)GetValue(ItemTemplateProperty); }
@@ -172,20 +146,18 @@
         /// <param name="bindable">The control</param>
         /// <param name="oldValue">Previous bound collection</param>
         /// <param name="newValue">New bound collection</param>
-        private static void ItemsChanged(BindableObject bindable, ObservableCollection<T> oldValue, ObservableCollection<T> newValue)
+        private static void ItemsChanged(BindableObject bindable, IEnumerable<T> oldValue, IEnumerable<T> newValue)
         {
             var control = bindable as RepeaterView<T>;
             if (control == null)
                 throw new Exception("Invalid bindable object passed to ReapterView::ItemsChanged expected a ReapterView<T> received a " + bindable.GetType().Name);
 
-            if (oldValue != null)
+            if (control._collectionChangedHandle != null)
             {
-                oldValue.CollectionChanged -= control.ItemsSource_CollectionChanged;
+                control._collectionChangedHandle.Dispose();
             }
 
-            newValue.CollectionChanged += control.ItemsSource_CollectionChanged;
-
-            
+            control._collectionChangedHandle = new CollectionChangedHandle(control, newValue);
             control.Children.Clear();
 
             foreach (var item in newValue)
@@ -201,7 +173,7 @@
         /// Update visible controls based on changes in the bound collection
         /// </summary>
         /// <param name="sender">Not used</param>
-        /// <param name="e"><see cref="NotifyCollectionChangedEventArgs"/></param>
+        /// <param name="e"></param>
         private void ItemsSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             if (e.Action == NotifyCollectionChangedAction.Reset)
@@ -210,21 +182,23 @@
             }
             else
             {
-                //Only one item can be removed from an Observable collection
-                //at any one time.
                 if (e.OldItems != null)
                 {
                     Children.RemoveAt(e.OldStartingIndex);
                 }
 
-                //Only one item can be added to an Observable collection
-                //at any one time
                 if (e.NewItems != null)
                 {
-                    var item = (T)e.NewItems[0];
-                    var view = ViewFor(item);
-                    Children.Insert(e.NewStartingIndex, view);
-                    NotifyItemAdded(view, item);
+                    foreach (T item in e.NewItems)
+                    {
+                        var comparer = EqualityComparer<T>.Default;
+                        var index = ItemsSource.Where(t => comparer.Equals(t, item))
+                                               .Select((t, i) => (int?)i)
+                                               .FirstOrDefault() ?? -1;
+                        var view = ViewFor(item);
+                        Children.Insert(index, view);
+                        NotifyItemAdded(view, item);
+                    }
                 }
             }
             UpdateChildrenLayout();
@@ -232,32 +206,16 @@
         }
     }
 
-    /// <summary>
-    /// Arguments passed to the <see cref="RepeaterView{T}.RepeaterViewItemAddedEventHandler"/>
-    /// </summary>
-    /// Element created at 10/11/2014,11:02 PM by Charles
     public class RepeaterViewItemAddedEventArgs : EventArgs
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RepeaterViewItemAddedEventArgs"/> class.
-        /// </summary>
-        /// <param name="view">The view.</param>
-        /// <param name="model">The model.</param>
-        /// Element created at 10/11/2014,11:03 PM by Charles
         public RepeaterViewItemAddedEventArgs(View view, object model)
         {
             View = view;
             Model = model;
         }
 
-        /// <summary>Gets or sets the view.</summary>
-        /// <value>The visual view.</value>
-        /// Element created at 10/11/2014,11:03 PM by Charles
         public View View { get; set; }
 
-        /// <summary>Gets or sets the model.</summary>
-        /// <value>The model bound to the view.</value>
-        /// Element created at 10/11/2014,11:03 PM by Charles
         public object Model { get; set; }
     }
 }
