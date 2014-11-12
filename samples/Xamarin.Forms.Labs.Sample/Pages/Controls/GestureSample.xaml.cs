8b431df66a4c4925c22c752aa124076ab6126a41
Sami M. Kallio <code.kallio@gmail.com>
2014-11-12 15:13:55 -0500

    Fix for iPhone6+ screen resolution

--- samples/Xamarin.Forms.Labs.Sample/Pages/Controls/GestureSample.xaml.cs
+++ samples/Xamarin.Forms.Labs.Sample/Pages/Controls/GestureSample.xaml.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace Xamarin.Forms.Labs.Sample.Pages.Controls
 {
-    using Xamarin.Forms.Labs.Sample.ViewModel;
-
     public partial class GestureSample  : ContentPage
     {
         public GestureSample()
