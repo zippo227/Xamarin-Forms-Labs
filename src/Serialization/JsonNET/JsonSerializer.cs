b3af9ac1ac920171a5e74fb08ff933fce2b5227e
ravensorb <sanderson@eye-catcher.com>
2014-12-31 15:46:49 -0500

    Added support for Contract Resolver

--- src/Serialization/JsonNET/JsonSerializer.cs
+++ src/Serialization/JsonNET/JsonSerializer.cs
@@ -4,85 +4,95 @@ using Newtonsoft.Json;
 
 namespace XLabs.Serialization.JsonNET
 {
-    /// <summary>
-    /// JSON serializer using Newtonsoft.Json library.
-    /// </summary>
-    /// <remarks>
-    /// 
-    /// Newtonsoft.Json copyright information.
-    /// 
-    /// The MIT License (MIT)
-    /// Copyright (c) 2007 James Newton-King
-    /// 
-    /// https://github.com/JamesNK/Newtonsoft.Json/blob/master/LICENSE.md
-    /// 
-    /// </remarks>
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1644:DocumentationHeadersMustNotContainBlankLines", Justification = "Reviewed. Suppression is OK here."), SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1629:DocumentationTextMustEndWithAPeriod", Justification = "Reviewed. Suppression is OK here."), SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
-    public class JsonSerializer : StringSerializer, IJsonSerializer
-    {
-        /// <summary>
-        /// Initializes a new instance of the
-        /// <see cref="Xamarin.Forms.Labs.Services.Serialization.JsonNET.JsonSerializer"/> classs.
-        /// </summary>
-        public JsonSerializer()
-        {
-        }
+	using Newtonsoft.Json.Serialization;
 
-        public JsonSerializer(TypeNameHandling typeNameHandling, ReferenceLoopHandling referenceLoopHandling, bool ignoreNulls = true)
-        {
-            JsonConvert.DefaultSettings = JsonConvert.DefaultSettings ?? new Func<JsonSerializerSettings>(() => 
-                new JsonSerializerSettings()
-                {
-                    TypeNameHandling = typeNameHandling,
-                    ReferenceLoopHandling = referenceLoopHandling,
-                    NullValueHandling = ignoreNulls ? NullValueHandling.Ignore : NullValueHandling.Include
-                });
-        }
+	/// <summary>
+	/// JSON serializer using Newtonsoft.Json library.
+	/// </summary>
+	/// <remarks>
+	/// 
+	/// Newtonsoft.Json copyright information.
+	/// 
+	/// The MIT License (MIT)
+	/// Copyright (c) 2007 James Newton-King
+	/// 
+	/// https://github.com/JamesNK/Newtonsoft.Json/blob/master/LICENSE.md
+	/// 
+	/// </remarks>
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1644:DocumentationHeadersMustNotContainBlankLines", Justification = "Reviewed. Suppression is OK here."), SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1629:DocumentationTextMustEndWithAPeriod", Justification = "Reviewed. Suppression is OK here."), SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+	public class JsonSerializer : StringSerializer, IJsonSerializer
+	{
 
-        /// <summary>
-        /// Gets the serialization format.
-        /// </summary>
-        /// <value>Serialization format.</value>
-        public override SerializationFormat Format
-        {
-            get { return SerializationFormat.Json; }
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="JsonSerializer"/> class.
+		/// </summary>
+		public JsonSerializer()
+		{
+		}
 
-        /// <summary>
-        /// Cleans memory.
-        /// </summary>
-        public override void Flush()
-        {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="JsonSerializer"/> class.
+		/// </summary>
+		/// <param name="typeNameHandling">The type name handling.</param>
+		/// <param name="referenceLoopHandling">The reference loop handling.</param>
+		/// <param name="ignoreNulls">if set to <c>true</c> [ignore nulls].</param>
+		/// <param name="contractResolver">The contract resolver.</param>
+		public JsonSerializer(TypeNameHandling typeNameHandling, ReferenceLoopHandling referenceLoopHandling, bool ignoreNulls = true, IContractResolver contractResolver = null)
+		{
+			JsonConvert.DefaultSettings = JsonConvert.DefaultSettings ?? new Func<JsonSerializerSettings>(() => 
+				new JsonSerializerSettings()
+				{
+					TypeNameHandling = typeNameHandling,
+					ReferenceLoopHandling = referenceLoopHandling,
+					NullValueHandling = ignoreNulls ? NullValueHandling.Ignore : NullValueHandling.Include,
+					ContractResolver = contractResolver
+				});
+		}
+
+		/// <summary>
+		/// Gets the serialization format.
+		/// </summary>
+		/// <value>Serialization format.</value>
+		public override SerializationFormat Format
+		{
+			get { return SerializationFormat.Json; }
+		}
+
+		/// <summary>
+		/// Cleans memory.
+		/// </summary>
+		public override void Flush()
+		{
 #if DEBUG
-            throw new NotImplementedException();
+			throw new NotImplementedException();
 #endif
-        }
+		}
 
-        /// <summary>
-        /// Serializes object to a string.
-        /// </summary>
-        /// <typeparam name="T">Type of object to serialize.</typeparam>
-        /// <param name="obj">Object to serialize.</param>
-        /// <returns>Serialized string of the object.</returns>
-        public override string Serialize<T>(T obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
+		/// <summary>
+		/// Serializes object to a string.
+		/// </summary>
+		/// <typeparam name="T">Type of object to serialize.</typeparam>
+		/// <param name="obj">Object to serialize.</param>
+		/// <returns>Serialized string of the object.</returns>
+		public override string Serialize<T>(T obj)
+		{
+			return JsonConvert.SerializeObject(obj);
+		}
 
-        /// <summary>
-        /// Deserializes string into an object.
-        /// </summary>
-        /// <typeparam name="T">Type of object to deserialize to.</typeparam>
-        /// <param name="data">Serialized object.</param>
-        /// <returns>Object of type T.</returns>
-        public override T Deserialize<T>(string data)
-        {
-            return JsonConvert.DeserializeObject<T>(data);
-        }
+		/// <summary>
+		/// Deserializes string into an object.
+		/// </summary>
+		/// <typeparam name="T">Type of object to deserialize to.</typeparam>
+		/// <param name="data">Serialized object.</param>
+		/// <returns>Object of type T.</returns>
+		public override T Deserialize<T>(string data)
+		{
+			return JsonConvert.DeserializeObject<T>(data);
+		}
 
-        public override object Deserialize(string data, Type type)
-        {
-            return JsonConvert.DeserializeObject(data, type);
-        }
-    }
+		public override object Deserialize(string data, Type type)
+		{
+			return JsonConvert.DeserializeObject(data, type);
+		}
+	}
 }
