12d9ff2b14208f1a0b982a50c32c96c193cfd9c1
Oren Novotny <oren@novotny.org>
2015-01-02 08:33:21 -0500

    Add a ContentPresenter control, works like it does in WPF/Microsoft XAML

---
+++ src/Forms/XLabs.Forms/Controls/ContentPresenter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XLabs.Forms.Controls
+{
+    /// <summary>
+    /// A view that renders its content based on a data template. Typical usage is to either set an explicit 
+    /// <see cref="BindableObject.BindingContext"/> on this element or use an inhereted one, then set a display.
+    /// </summary>
+    public class ContentPresenter : ContentView
+    {
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create("ItemTemplate", typeof(DataTemplate), typeof(ContentPresenter), null, propertyChanged: OnItemTemplateChanged);
+
+        private static void OnItemTemplateChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            var cp = (ContentPresenter)bindable;
+
+            var template = cp.ItemTemplate;
+            if (template != null)
+            {
+                var content = (View)template.CreateContent();
+                cp.Content = content;
+            }
+            else
+            {
+                cp.Content = null;
+            }
+        }
+
+        /// <summary>
+        /// A <see cref="DataTemplate"/> used to render the view. This property is bindable.
+        /// </summary>
+        public DataTemplate ItemTemplate
+        {
+            get
+            {
+                return (DataTemplate)GetValue(ItemTemplateProperty);
+            }
+            set
+            {
+                SetValue(ItemTemplateProperty, value);
+            }
+        }
+
+    }
+}
