694a76c06945b3a8ebe5766dfe5c78e0012acc1f
Sami M. Kallio <code.kallio@gmail.com>
2015-01-25 19:53:40 -0500

    String extension update for iOS

--- src/Platform/XLabs.Platform.iOS/Extensions/StringExtensions.cs
+++ src/Platform/XLabs.Platform.iOS/Extensions/StringExtensions.cs
@@ -13,23 +13,33 @@ namespace XLabs.Platform.Extensions
 	public static class StringExtensions
 	{
 		/// <summary>
-		/// Strings the height.
+		/// Gets the height of a string.
 		/// </summary>
 		/// <param name="text">The text.</param>
 		/// <param name="font">The font.</param>
 		/// <param name="width">The width.</param>
-		/// <returns>System.Single.</returns>
+        /// <returns>nfloat.</returns>
 		public static nfloat StringHeight(this string text, UIFont font, nfloat width)
 		{
-			var nativeString = new NSString(text);
-
-			var rect = nativeString.GetBoundingRect(
-				new CGSize(width, float.MaxValue),
-				NSStringDrawingOptions.UsesLineFragmentOrigin,
-				new UIStringAttributes { Font = font },
-				null);
-
-			return rect.Height;
+            return text.StringRect(font, width).Height;
 		}
+
+        /// <summary>
+        /// Gets the rectangle of a string.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="font">The font.</param>
+        /// <param name="width">The width.</param>
+        /// <returns>CGRect.</returns>
+	    public static CGRect StringRect(this string text, UIFont font, nfloat width)
+	    {
+            var nativeString = new NSString(text);
+
+            return nativeString.GetBoundingRect(
+                new CGSize(width, float.MaxValue),
+                NSStringDrawingOptions.UsesLineFragmentOrigin,
+                new UIStringAttributes { Font = font },
+                null);
+	    }
 	}
 }
