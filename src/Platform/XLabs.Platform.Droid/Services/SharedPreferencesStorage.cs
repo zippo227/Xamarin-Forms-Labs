a787e351f833fbb1d787c3e87b477326415d008d
Sami M. Kallio <code.kallio@gmail.com>
2015-02-05 23:34:14 -0500

    Fixed SharedPreferences implementation.

--- src/Platform/XLabs.Platform.Droid/Services/SharedPreferencesStorage.cs
+++ src/Platform/XLabs.Platform.Droid/Services/SharedPreferencesStorage.cs
@@ -1,7 +1,6 @@
 ﻿namespace XLabs.Platform.Services
 {
     using System.Text;
-    using System.Threading;
     using Android.App;
     using Android.Content;
 
@@ -11,8 +10,6 @@
     public class SharedPreferencesStorage : ISecureStorage
     {
         private readonly ISharedPreferences preferences;
-        private readonly string preferenceKey;
-
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SharedPreferencesStorage"/> class.
@@ -20,7 +17,6 @@
         /// <param name="preferenceKey">Preferences key to use.</param>
         public SharedPreferencesStorage(string preferenceKey)
         {
-            this.preferenceKey = preferenceKey;
             this.preferences = Application.Context.GetSharedPreferences(preferenceKey, FileCreationMode.Private);
         }
 
@@ -33,20 +29,10 @@
         /// <param name="dataBytes">Data bytes to store.</param>
         public void Store(string key, byte[] dataBytes)
         {
-            var mutex = GetMutex(key);
-
-            try
+            using (var editor = this.preferences.Edit())
             {
-                mutex.WaitOne();
-                using (var editor = this.preferences.Edit())
-                {
-                    editor.PutString(key, Encoding.UTF8.GetString(dataBytes));
-                    editor.Commit();
-                }
-            }
-            finally
-            {
-                mutex.ReleaseMutex();
+                editor.PutString(key, Encoding.UTF8.GetString(dataBytes));
+                editor.Commit();
             }
         }
 
@@ -57,17 +43,7 @@
         /// <returns>Byte array of stored data.</returns>
         public byte[] Retrieve(string key)
         {
-            var mutex = GetMutex(key);
-
-            try
-            {
-                mutex.WaitOne();
-                return Encoding.UTF8.GetBytes(this.preferences.GetString(key, string.Empty));
-            }
-            finally
-            {
-                mutex.ReleaseMutex();
-            }
+            return Encoding.UTF8.GetBytes(this.preferences.GetString(key, string.Empty));
         }
 
         /// <summary>
@@ -76,34 +52,16 @@
         /// <param name="key">Key for the data to be deleted.</param>
         public void Delete(string key)
         {
-            var mutex = GetMutex(key);
-
-            try
+            if (!this.preferences.Contains(key))
             {
-                mutex.WaitOne();
-                if (!this.preferences.Contains(key))
-                {
-                    return;
-                }
-                using (var editor = this.preferences.Edit())
-                {
-                    editor.Remove(key);
-                    editor.Commit();
-                }
+                return;
             }
-            finally
+
+            using (var editor = this.preferences.Edit())
             {
-                mutex.ReleaseMutex();
+                editor.Remove(key);
+                editor.Commit();
             }
-        }
-
-        #endregion
-
-        #region private methods
-
-        private Mutex GetMutex(string key)
-        {
-            return new Mutex(false, this.preferenceKey + key);
         }
 
         #endregion
