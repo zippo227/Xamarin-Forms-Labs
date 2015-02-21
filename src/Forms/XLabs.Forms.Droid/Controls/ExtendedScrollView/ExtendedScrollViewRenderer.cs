6cfe2c0e9c91a7669d151cd99d738018d7cc6f57
Sami M. Kallio <skallio@Samis-MacBook-Pro.local>
2015-02-20 19:41:37 -0500

    Fix for Android ExtendedScrollViewRenderer

--- src/Forms/XLabs.Forms.Droid/Controls/ExtendedScrollView/ExtendedScrollViewRenderer.cs
+++ src/Forms/XLabs.Forms.Droid/Controls/ExtendedScrollView/ExtendedScrollViewRenderer.cs
@@ -1,69 +1,72 @@
 ﻿using Xamarin.Forms;
-
 using XLabs.Forms.Controls;
 
 [assembly: ExportRenderer(typeof(ExtendedScrollView), typeof(ExtendedScrollViewRenderer))]
+
 namespace XLabs.Forms.Controls
 {
-	using System;
-	using Android.Views;
-	using Xamarin.Forms;
-	using Xamarin.Forms.Platform.Android;
+    using System;
+    using System.ComponentModel;
+    using Xamarin.Forms.Platform.Android;
 
-	/// <summary>
-	/// Class ExtendedScrollViewRenderer.
-	/// </summary>
-	public class ExtendedScrollViewRenderer:ScrollViewRenderer
-	{
-		/// <summary>
-		/// Handles the <see cref="E:ElementChanged" /> event.
-		/// </summary>
-		/// <param name="e">The <see cref="VisualElementChangedEventArgs"/> instance containing the event data.</param>
-		protected override void OnElementChanged(VisualElementChangedEventArgs e)
-		{
-			base.OnElementChanged(e);
+    /// <summary>
+    /// Class ExtendedScrollViewRenderer.
+    /// </summary>
+    public class ExtendedScrollViewRenderer:ScrollViewRenderer
+    {
+        /// <summary>
+        /// The epsilon.
+        /// </summary>
+        private const double Epsilon = 0.1;
 
-			ViewTreeObserver.ScrollChanged += (sender, ev) => {
-				var scrollView = (ExtendedScrollView)Element;
-				if(scrollView == null)
-					return;
+        /// <summary>
+        /// Handles the <see cref="E:ElementChanged" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="VisualElementChangedEventArgs"/> instance containing the event data.</param>
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
 
-				var bounds = new Rectangle(ScrollX, ScrollY, GetChildAt(0).Width, GetChildAt(0).Height);
-				scrollView.UpdateBounds(bounds);
-			};
+            this.ViewTreeObserver.ScrollChanged += (sender, ev) => {
+                var scrollView = (ExtendedScrollView)this.Element;
+                if(scrollView == null)
+                    return;
 
-			if(e.OldElement != null)
-				e.OldElement.PropertyChanged -= OnElementPropertyChanged;
+                var bounds = new Rectangle(this.ScrollX, this.ScrollY, GetChildAt(0).Width, GetChildAt(0).Height);
+                scrollView.UpdateBounds(bounds);
+            };
 
-			e.NewElement.PropertyChanged += OnElementPropertyChanged;
-		}
+            if(e.OldElement != null)
+                e.OldElement.PropertyChanged -= OnElementPropertyChanged;
 
-		/// <summary>
-		/// The epsilon
-		/// </summary>
-		double _epsilon = 0.1;
+            e.NewElement.PropertyChanged += OnElementPropertyChanged;
+        }
 
-		/// <summary>
-		/// Handles the <see cref="E:ElementPropertyChanged" /> event.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
-		protected void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == ExtendedScrollView.PositionProperty.PropertyName)
-			{
-				var scrollView = (ExtendedScrollView)Element;
-				var position = scrollView.Position;
+        /// <summary>
+        /// Handles the <see cref="E:ElementPropertyChanged" /> event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
+        protected void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != ExtendedScrollView.PositionProperty.PropertyName)
+            {
+                return;
+            }
 
-				if (Math.Abs((int)(ScrollY - position.Y)) < _epsilon
-					&& Math.Abs((int)(ScrollX - position.X)) < _epsilon)
-					return;
+            var scrollView = (ExtendedScrollView)this.Element;
+            var position = scrollView.Position;
 
-				ScrollTo((int)position.X,(int)position.Y);
-				UpdateLayout();
-			}
-		}
-	}
+            if (Math.Abs((int) (this.ScrollY - position.Y)) < Epsilon
+                && Math.Abs((int) (this.ScrollX - position.X)) < Epsilon)
+            {
+                return;
+            }
+
+            ScrollTo((int)position.X,(int)position.Y);
+            UpdateLayout();
+        }
+    }
 
 }
 
