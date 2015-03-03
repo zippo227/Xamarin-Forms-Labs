cf00d6c67e4125a1be437495414be06a43fa2e0c
Sami M. Kallio <code.kallio@gmail.com>
2015-03-02 23:25:21 -0500

    Update to Xamarin Forms 1.3.5.6335

--- src/Forms/Charting/XLabs.Forms.Charting.iOS/Controls/ChartSurface.cs
+++ src/Forms/Charting/XLabs.Forms.Charting.iOS/Controls/ChartSurface.cs
@@ -1,184 +1,181 @@
 namespace XLabs.Forms.Charting.Controls
 {
-	using System;
-	using CoreGraphics;
+    using System;
+    using CoreGraphics;
+    using Events;
+    using Foundation;
+    using UIKit;
 
-	using CoreGraphics;
-	using Foundation;
-	using UIKit;
+    /// <summary>
+    /// Class ChartSurface.
+    /// </summary>
+    public class ChartSurface : UIView
+    {
+        const float StartAngle = -((float)Math.PI / 2);
+        const float EndAngle = ((2 * (float)Math.PI) + StartAngle);
 
-	using XLabs.Forms.Charting.Events;
+        /// <summary>
+        /// The chart
+        /// </summary>
+        internal Chart Chart;
 
-	/// <summary>
-	/// Class ChartSurface.
-	/// </summary>
-	public class ChartSurface : UIView
-	{
-		/// <summary>
-		/// The chart
-		/// </summary>
-		public Chart Chart;
-		/// <summary>
-		/// The color
-		/// </summary>
-		public UIColor Color;
-		/// <summary>
-		/// The colors
-		/// </summary>
-		public UIColor[] Colors;
+        /// <summary>
+        /// The color
+        /// </summary>
+        internal UIColor Color;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ChartSurface"/> class.
-		/// </summary>
-		/// <param name="chart">The chart.</param>
-		/// <param name="color">The color.</param>
-		/// <param name="colors">The colors.</param>
-		public ChartSurface(Chart chart, UIColor color, UIColor[] colors)
-		{
-			Chart = chart;
-			Color = color;
-			Colors = colors;
+        /// <summary>
+        /// The colors
+        /// </summary>
+        internal UIColor[] Colors;
 
-			Chart.OnDrawBar -= _chart_OnDrawBar;
-			Chart.OnDrawBar += _chart_OnDrawBar;
-			Chart.OnDrawCircle -= _chart_OnDrawCircle;
-			Chart.OnDrawCircle += _chart_OnDrawCircle;
-			Chart.OnDrawGridLine -= _chart_OnDrawGridLine;
-			Chart.OnDrawGridLine += _chart_OnDrawGridLine;
-			Chart.OnDrawLine -= _chart_OnDrawLine;
-			Chart.OnDrawLine += _chart_OnDrawLine;
-			Chart.OnDrawText -= _chart_OnDrawText;
-			Chart.OnDrawText += _chart_OnDrawText;
-			Chart.OnDrawPie -= _chart_OnDrawPie;
-			Chart.OnDrawPie += _chart_OnDrawPie;
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChartSurface"/> class.
+        /// </summary>
+        /// <param name="chart">The chart.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="colors">The colors.</param>
+        public ChartSurface(Chart chart, UIColor color, UIColor[] colors)
+        {
+            this.Chart = chart;
+            this.Color = color;
+            this.Colors = colors;
 
-		/// <summary>
-		/// Draws the specified rect.
-		/// </summary>
-		/// <param name="rect">The rect.</param>
-		public override void Draw(CGRect rect)
-		{
-			base.Draw(rect);
+            this.Chart.OnDrawBar -= _chart_OnDrawBar;
+            this.Chart.OnDrawBar += _chart_OnDrawBar;
+            this.Chart.OnDrawCircle -= _chart_OnDrawCircle;
+            this.Chart.OnDrawCircle += _chart_OnDrawCircle;
+            this.Chart.OnDrawGridLine -= _chart_OnDrawGridLine;
+            this.Chart.OnDrawGridLine += _chart_OnDrawGridLine;
+            this.Chart.OnDrawLine -= _chart_OnDrawLine;
+            this.Chart.OnDrawLine += _chart_OnDrawLine;
+            this.Chart.OnDrawText -= _chart_OnDrawText;
+            this.Chart.OnDrawText += _chart_OnDrawText;
+            this.Chart.OnDrawPie -= _chart_OnDrawPie;
+            this.Chart.OnDrawPie += _chart_OnDrawPie;
+        }
 
-			Chart.DrawChart();
-		}
+        /// <summary>
+        /// Draws the specified rect.
+        /// </summary>
+        /// <param name="rect">The rect.</param>
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
 
-		/// <summary>
-		/// _chart_s the on draw bar.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The e.</param>
-		private void _chart_OnDrawBar(object sender, Chart.DrawEventArgs<DoubleDrawingData> e)
-		{
-			using (CGContext g = UIGraphics.GetCurrentContext())
-			{
-				g.SetLineWidth(1);
-				Colors[e.Data.SeriesNo].SetFill();
-				Colors[e.Data.SeriesNo].SetStroke();
+            this.Chart.DrawChart();
+        }
 
-				CGRect rect = new CGRect((float)e.Data.XFrom, (float)e.Data.YFrom, (float)(e.Data.XTo - e.Data.XFrom), (float)(e.Data.YTo - e.Data.YFrom));
-				g.AddRect(rect);
+        /// <summary>
+        /// _chart_s the on draw bar.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void _chart_OnDrawBar(object sender, Chart.DrawEventArgs<DoubleDrawingData> e)
+        {
+            using (var g = UIGraphics.GetCurrentContext())
+            {
+                g.SetLineWidth(1);
+                this.Colors[e.Data.SeriesNo].SetFill();
+                this.Colors[e.Data.SeriesNo].SetStroke();
 
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-			}
-		}
+                var rect = new CGRect((float)e.Data.XFrom, (float)e.Data.YFrom, (float)(e.Data.XTo - e.Data.XFrom), (float)(e.Data.YTo - e.Data.YFrom));
+                g.AddRect(rect);
 
-		/// <summary>
-		/// _chart_s the on draw circle.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The e.</param>
-		private void _chart_OnDrawCircle(object sender, Chart.DrawEventArgs<SingleDrawingData> e)
-		{
-			using (CGContext g = UIGraphics.GetCurrentContext())
-			{
-				g.SetLineWidth(2);
-				Colors[e.Data.SeriesNo].SetFill();
-				Colors[e.Data.SeriesNo].SetStroke();
+                g.DrawPath(CGPathDrawingMode.FillStroke);
+            }
+        }
 
-				float startAngle = -((float)Math.PI / 2);
-				float endAngle = ((2 * (float)Math.PI) + startAngle);
-				g.AddArc((float)e.Data.X, (float)e.Data.Y, (float)e.Data.Size, startAngle, endAngle, true);
+        /// <summary>
+        /// _chart_s the on draw circle.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void _chart_OnDrawCircle(object sender, Chart.DrawEventArgs<SingleDrawingData> e)
+        {
+            using (var g = UIGraphics.GetCurrentContext())
+            {
+                g.SetLineWidth(2);
+                this.Colors[e.Data.SeriesNo].SetFill();
+                this.Colors[e.Data.SeriesNo].SetStroke();
+                g.AddArc((float)e.Data.X, (float)e.Data.Y, (float)e.Data.Size, StartAngle, EndAngle, true);
+                g.DrawPath(CGPathDrawingMode.FillStroke);
+            }
+        }
 
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-			}
-		}
+        /// <summary>
+        /// _chart_s the on draw grid line.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void _chart_OnDrawGridLine(object sender, Chart.DrawEventArgs<DoubleDrawingData> e)
+        {
+            using (var g = UIGraphics.GetCurrentContext())
+            {
+                g.SetLineWidth(2);
+                this.Color.SetFill();
+                this.Color.SetStroke();
 
-		/// <summary>
-		/// _chart_s the on draw grid line.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The e.</param>
-		private void _chart_OnDrawGridLine(object sender, Chart.DrawEventArgs<DoubleDrawingData> e)
-		{
-			using (CGContext g = UIGraphics.GetCurrentContext())
-			{
-				g.SetLineWidth(2);
-				Color.SetFill();
-				Color.SetStroke();
+                g.MoveTo((float)e.Data.XFrom, (float)e.Data.YFrom);
+                g.AddLineToPoint((float)e.Data.XTo, (float)e.Data.YTo);
 
-				g.MoveTo((float)e.Data.XFrom, (float)e.Data.YFrom);
-				g.AddLineToPoint((float)e.Data.XTo, (float)e.Data.YTo);
+                g.DrawPath(CGPathDrawingMode.FillStroke);
+            }
+        }
 
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-			}
-		}
+        /// <summary>
+        /// _chart_s the on draw line.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void _chart_OnDrawLine(object sender, Chart.DrawEventArgs<DoubleDrawingData> e)
+        {
+            using (var g = UIGraphics.GetCurrentContext())
+            {
+                g.SetLineWidth(2.5F);
+                this.Colors[e.Data.SeriesNo].SetFill();
+                this.Colors[e.Data.SeriesNo].SetStroke();
 
-		/// <summary>
-		/// _chart_s the on draw line.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The e.</param>
-		private void _chart_OnDrawLine(object sender, Chart.DrawEventArgs<DoubleDrawingData> e)
-		{
-			using (CGContext g = UIGraphics.GetCurrentContext())
-			{
-				g.SetLineWidth(2.5F);
-				Colors[e.Data.SeriesNo].SetFill();
-				Colors[e.Data.SeriesNo].SetStroke();
+                g.MoveTo((float)e.Data.XFrom, (float)e.Data.YFrom);
+                g.AddLineToPoint((float)e.Data.XTo, (float)e.Data.YTo);
 
-				g.MoveTo((float)e.Data.XFrom, (float)e.Data.YFrom);
-				g.AddLineToPoint((float)e.Data.XTo, (float)e.Data.YTo);
+                g.DrawPath(CGPathDrawingMode.FillStroke);
+            }
+        }
 
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-			}
-		}
+        /// <summary>
+        /// _chart_s the on draw pie.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void _chart_OnDrawPie(object sender, Chart.DrawEventArgs<PieDrawingData> e)
+        {
+            double totalDegrees = 0;
+            for (var i = 0; i < e.Data.Percentages.Length; i++)
+            {
+                var degrees = e.Data.Percentages[i];
+                using (var g = UIGraphics.GetCurrentContext())
+                {
+                    g.SetLineWidth(2);
+                    this.Colors[i].SetFill();
+                    this.Colors[i].SetStroke();
+                    g.AddArc((float)e.Data.X, (float)e.Data.Y, (float)e.Data.Size, (float)(Math.PI / 180 * totalDegrees), (float)(Math.PI / 180 * degrees), true);
+                    g.DrawPath(CGPathDrawingMode.FillStroke);
+                }
 
-		/// <summary>
-		/// _chart_s the on draw text.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The e.</param>
-		private void _chart_OnDrawText(object sender, Chart.DrawEventArgs<TextDrawingData> e)
-		{
-			NSString str = new NSString(e.Data.Text);
-			str.DrawString(new CGPoint((float)e.Data.X, (float)e.Data.Y), UIFont.SystemFontOfSize(12));
-		}
+                totalDegrees += degrees;
+            }
+        }
 
-		/// <summary>
-		/// _chart_s the on draw pie.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The e.</param>
-		private void _chart_OnDrawPie(object sender, Chart.DrawEventArgs<PieDrawingData> e)
-		{
-			double totalDegrees = 0;
-			for (int i = 0; i < e.Data.Percentages.Length; i++)
-			{
-				double degrees = e.Data.Percentages[i];
-				using (CGContext g = UIGraphics.GetCurrentContext())
-				{
-					g.SetLineWidth(2);
-					Colors[i].SetFill();
-					Colors[i].SetStroke();
-
-					g.AddArc((float)e.Data.X, (float)e.Data.Y, (float)e.Data.Size, (float)(Math.PI / 180 * totalDegrees), (float)(Math.PI / 180 * degrees), true);
-
-					g.DrawPath(CGPathDrawingMode.FillStroke);
-				}
-				totalDegrees += degrees;
-			}
-		}
-	}
+        /// <summary>
+        /// _chart_s the on draw text.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private static void _chart_OnDrawText(object sender, Chart.DrawEventArgs<TextDrawingData> e)
+        {
+            var str = new NSString(e.Data.Text);
+            str.DrawString(new CGPoint((float)e.Data.X, (float)e.Data.Y), UIFont.SystemFontOfSize(12));
+        }
+    }
 }
