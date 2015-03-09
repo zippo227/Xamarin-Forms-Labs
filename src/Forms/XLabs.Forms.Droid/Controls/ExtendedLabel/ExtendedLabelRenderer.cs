9c719f4e6973ffc0e4edfd36f2ef287f8bf0abc3
Alex Lau <alexlau811@users.noreply.github.com>
2015-03-09 21:10:23 +0800

    Revert "Fixes to ExtendedLabel, remove font related properties"

--- src/Forms/XLabs.Forms.Droid/Controls/ExtendedLabel/ExtendedLabelRenderer.cs
+++ src/Forms/XLabs.Forms.Droid/Controls/ExtendedLabel/ExtendedLabelRenderer.cs
@@ -35,33 +35,11 @@ namespace XLabs.Forms.Controls
 		{
 			base.OnElementChanged(e);
 
-			if (e.OldElement == null && e.NewElement != null) {
+			var view = (ExtendedLabel)Element;
+			var control = Control;
 
-				var view = (ExtendedLabel) e.NewElement;
-				var control = Control;
+			UpdateUi(view, control);
 
-				UpdateUi (view, control);
-			}
-
-		}
-
-		/// <summary>
-		/// Raises the element property changed event.
-		/// </summary>
-		/// <param name="sender">Sender</param>
-		/// <param name="e">The event arguments</param>
-		protected override void OnElementPropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			base.OnElementPropertyChanged (sender, e);
-
-			if (e.PropertyName == ExtendedLabel.IsUnderlineProperty.PropertyName ||
-				e.PropertyName == ExtendedLabel.IsDropShadowProperty.PropertyName ||
-				e.PropertyName == ExtendedLabel.IsStrikeThroughProperty.PropertyName
-				) {
-				var view = (ExtendedLabel) Element;
-				var control = Control;
-				UpdateUi (view,control);
-			}
 		}
 
 		/// <summary>
@@ -71,8 +49,34 @@ namespace XLabs.Forms.Controls
 		/// <param name="control">The control.</param>
 		void UpdateUi(ExtendedLabel view, TextView control)
 		{
-			if (view == null || control == null)
-				return;
+			if(!string.IsNullOrEmpty(view.FontName))
+			{
+				string filename = view.FontName;
+				//if no extension given then assume and add .ttf
+				if(filename.LastIndexOf(".", System.StringComparison.Ordinal) != filename.Length - 4)
+				{
+					filename = string.Format("{0}.ttf", filename);
+				}
+				control.Typeface = TrySetFont(filename);
+			}
+
+			//======= This is for backward compatability with obsolete attrbute 'FontNameAndroid' ========
+			else if(!string.IsNullOrEmpty(view.FontNameAndroid))
+			{
+				control.Typeface = TrySetFont(view.FontNameAndroid);
+
+			}
+			//====== End of obsolete section ==========================================================
+
+			else if(view.Font != Font.Default)
+			{
+				control.Typeface = view.Font.ToExtendedTypeface(Context);
+			}
+
+			if(view.FontSize > 0)
+			{
+				control.TextSize = (float)view.FontSize;
+			}
 
 			if(view.IsUnderline)
 			{
@@ -84,10 +88,31 @@ namespace XLabs.Forms.Controls
 				control.PaintFlags = control.PaintFlags | PaintFlags.StrikeThruText;
 			}
 
-			if (view.IsDropShadow) {
-				//TODO:: Needs implementation
-			}
+		}
 
+		/// <summary>
+		/// Tries the set font.
+		/// </summary>
+		/// <param name="fontName">Name of the font.</param>
+		/// <returns>Typeface.</returns>
+		private Typeface TrySetFont(string fontName)
+		{
+			try
+			{                
+				return Typeface.CreateFromAsset(Context.Assets, "fonts/" + fontName);
+			} catch(Exception ex)
+			{
+				Console.WriteLine("not found in assets. Exception: {0}", ex);
+				try
+				{
+					return Typeface.CreateFromFile("fonts/" + fontName);
+				} catch(Exception ex1)
+				{
+					Console.WriteLine("not found by file. Exception: {0}", ex1);
+
+					return Typeface.Default;
+				}
+			}
 		}
 	}
 }
