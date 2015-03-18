3645c88830d4d4e7dad6ea65173c49a7b1333069
Sami M. Kallio <skallio@Samis-MacBook-Pro-2.local>
2015-03-18 11:20:21 -0400

    Added IReport interface + static Report class Default Debug.WriteLine implementation + Plugin for Xamarin.Insights planned.

---
+++ src/XLabs.Core/Reporting/IReport.cs
@@ -0,0 +1,16 @@
+﻿namespace XLabs.Reporting
+{
+    using System;
+
+    /// <summary>
+    /// Interface for reporting events.
+    /// </summary>
+    public interface IReport
+    {
+        /// <summary>
+        /// Report an exception.
+        /// </summary>
+        /// <param name="exception">Exception that happened.</param>
+        void Exception(Exception exception);
+    }
+}
