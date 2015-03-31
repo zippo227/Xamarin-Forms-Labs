2e4ffd288e165f7c287f5e4793cff577e779bf0e
Sami M. Kallio <skallio@Samis-MacBook-Pro-2.local>
2015-03-31 10:34:18 -0400

    Added disabled image support for ImageButton

--- src/Forms/XLabs.Forms.WP8/Controls/ImageButton/ImageButtonRenderer.cs
+++ src/Forms/XLabs.Forms.WP8/Controls/ImageButton/ImageButtonRenderer.cs
@@ -6,191 +6,199 @@ using XLabs.Forms.Controls;
 
 namespace XLabs.Forms.Controls
 {
-	using System;
-	using System.ComponentModel;
-	using System.Threading.Tasks;
-	using System.Windows;
-	using System.Windows.Controls;
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
 
-	using Xamarin.Forms;
-	using Xamarin.Forms.Platform.WinPhone;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Platform.WinPhone;
 
-	using XLabs.Enums;
+    using Enums;
 
-	using Button = Xamarin.Forms.Button;
-	using Image = System.Windows.Controls.Image;
-	using Orientation = System.Windows.Controls.Orientation;
-	using TextAlignment = System.Windows.TextAlignment;
-	using Thickness = System.Windows.Thickness;
+    using Button = Button;
+    using Image = System.Windows.Controls.Image;
+    using Orientation = System.Windows.Controls.Orientation;
+    using TextAlignment = System.Windows.TextAlignment;
+    using Thickness = System.Windows.Thickness;
 
-	/// <summary>
-	///     Draws a button on the Windows Phone platform with the image shown in the right
-	///     position with the right size.
-	/// </summary>
-	public partial class ImageButtonRenderer : ButtonRenderer
-	{
-		/// <summary>
-		///     The image displayed in the button.
-		/// </summary>
-		private Image _currentImage;
+    /// <summary>
+    ///     Draws a button on the Windows Phone platform with the image shown in the right
+    ///     position with the right size.
+    /// </summary>
+    public partial class ImageButtonRenderer : ButtonRenderer
+    {
+        /// <summary>
+        ///     The image displayed in the button.
+        /// </summary>
+        private Image _currentImage;
 
-		/// <summary>
-		///     Handles the initial drawing of the button.
-		/// </summary>
-		/// <param name="e">Information on the <see cref="ImageButton" />.</param>
-		protected override async void OnElementChanged(ElementChangedEventArgs<Button> e)
-		{
-			base.OnElementChanged(e);
+        /// <summary>
+        ///     Handles the initial drawing of the button.
+        /// </summary>
+        /// <param name="e">Information on the <see cref="ImageButton" />.</param>
+        protected override async void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
 
-			var sourceButton = Element as ImageButton;
-			var targetButton = Control;
-			if (sourceButton != null && targetButton != null && sourceButton.Source != null)
-			{
-				var stackPanel = new StackPanel
-					                 {
-						                 Orientation =
-							                 (sourceButton.Orientation == ImageOrientation.ImageOnTop
-							                  || sourceButton.Orientation == ImageOrientation.ImageOnBottom)
-								                 ? Orientation.Vertical
-								                 : Orientation.Horizontal
-					                 };
+            var sourceButton = Element as ImageButton;
+            var targetButton = Control;
+            if (sourceButton != null && targetButton != null && sourceButton.Source != null)
+            {
+                var stackPanel = new StackPanel
+                                     {
+                                         Orientation =
+                                             (sourceButton.Orientation == ImageOrientation.ImageOnTop
+                                              || sourceButton.Orientation == ImageOrientation.ImageOnBottom)
+                                                 ? Orientation.Vertical
+                                                 : Orientation.Horizontal
+                                     };
 
-				_currentImage =
-					await
-					GetImageAsync(
-						sourceButton.Source,
-						GetHeight(sourceButton.ImageHeightRequest),
-						GetWidth(sourceButton.ImageWidthRequest));
-				SetImageMargin(_currentImage, sourceButton.Orientation);
+                _currentImage = await GetCurrentImage();
+                SetImageMargin(_currentImage, sourceButton.Orientation);
 
-				var label = new TextBlock
-					            {
-						            TextAlignment = GetTextAlignment(sourceButton.Orientation),
-						            FontSize = 16,
-						            VerticalAlignment = VerticalAlignment.Center,
-						            Text = sourceButton.Text
-					            };
+                var label = new TextBlock
+                                {
+                                    TextAlignment = GetTextAlignment(sourceButton.Orientation),
+                                    FontSize = 16,
+                                    VerticalAlignment = VerticalAlignment.Center,
+                                    Text = sourceButton.Text
+                                };
 
-				if (sourceButton.Orientation == ImageOrientation.ImageToLeft)
-				{
-					targetButton.HorizontalContentAlignment = HorizontalAlignment.Left;
-				}
-				else if (sourceButton.Orientation == ImageOrientation.ImageToRight)
-				{
-					targetButton.HorizontalContentAlignment = HorizontalAlignment.Right;
-				}
+                if (sourceButton.Orientation == ImageOrientation.ImageToLeft)
+                {
+                    targetButton.HorizontalContentAlignment = HorizontalAlignment.Left;
+                }
+                else if (sourceButton.Orientation == ImageOrientation.ImageToRight)
+                {
+                    targetButton.HorizontalContentAlignment = HorizontalAlignment.Right;
+                }
 
-				if (sourceButton.Orientation == ImageOrientation.ImageOnTop
-				    || sourceButton.Orientation == ImageOrientation.ImageToLeft)
-				{
-					_currentImage.HorizontalAlignment = HorizontalAlignment.Left;
-					stackPanel.Children.Add(_currentImage);
-					stackPanel.Children.Add(label);
-				}
-				else
-				{
-					stackPanel.Children.Add(label);
-					stackPanel.Children.Add(_currentImage);
-				}
+                if (sourceButton.Orientation == ImageOrientation.ImageOnTop
+                    || sourceButton.Orientation == ImageOrientation.ImageToLeft)
+                {
+                    _currentImage.HorizontalAlignment = HorizontalAlignment.Left;
+                    stackPanel.Children.Add(_currentImage);
+                    stackPanel.Children.Add(label);
+                }
+                else
+                {
+                    stackPanel.Children.Add(label);
+                    stackPanel.Children.Add(_currentImage);
+                }
 
-				targetButton.Content = stackPanel;
-			}
-		}
+                targetButton.Content = stackPanel;
+            }
+        }
 
-		/// <summary>
-		///     Called when the underlying model's properties are changed.
-		/// </summary>
-		/// <param name="sender">Model sending the change event.</param>
-		/// <param name="e">Event arguments.</param>
-		protected override async void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			base.OnElementPropertyChanged(sender, e);
+        /// <summary>
+        ///     Called when the underlying model's properties are changed.
+        /// </summary>
+        /// <param name="sender">Model sending the change event.</param>
+        /// <param name="e">Event arguments.</param>
+        protected override async void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
 
-			if (e.PropertyName == ImageButton.SourceProperty.PropertyName)
-			{
-				var sourceButton = Element as ImageButton;
-				if (sourceButton != null && sourceButton.Source != null)
-				{
-					_currentImage =
-						await GetImageAsync(sourceButton.Source, sourceButton.ImageHeightRequest, sourceButton.ImageWidthRequest);
-					SetImageMargin(_currentImage, sourceButton.Orientation);
-				}
-			}
-		}
+            if (e.PropertyName != ImageButton.SourceProperty.PropertyName &&
+                e.PropertyName != ImageButton.DisabledSourceProperty.PropertyName)
+            {
+                return;
+            }
 
-		/// <summary>
-		///     Returns the alignment of the label on the button depending on the orientation.
-		/// </summary>
-		/// <param name="imageOrientation">The orientation to use.</param>
-		/// <returns>The alignment to use for the text.</returns>
-		private static TextAlignment GetTextAlignment(ImageOrientation imageOrientation)
-		{
-			TextAlignment returnValue;
-			switch (imageOrientation)
-			{
-				case ImageOrientation.ImageToLeft:
-					returnValue = TextAlignment.Left;
-					break;
-				case ImageOrientation.ImageToRight:
-					returnValue = TextAlignment.Right;
-					break;
-				default:
-					returnValue = TextAlignment.Center;
-					break;
-			}
+            var sourceButton = this.Element as ImageButton;
+            if (sourceButton != null)
+            {
+                this._currentImage = await GetCurrentImage();
+                SetImageMargin(this._currentImage, sourceButton.Orientation);
+            }
+        }
 
-			return returnValue;
-		}
+        private Task<Image> GetCurrentImage()
+        {
+            var sourceButton = this.Element as ImageButton;
+            if (sourceButton == null) return null;
 
-		/// <summary>
-		///     Returns a <see cref="Xamarin.Forms.Image" /> from the <see cref="ImageSource" /> provided.
-		/// </summary>
-		/// <param name="source">The <see cref="ImageSource" /> to load the image from.</param>
-		/// <param name="height">The height for the image (divides by 2 for the Windows Phone platform).</param>
-		/// <param name="width">The width for the image (divides by 2 for the Windows Phone platform).</param>
-		/// <returns>A properly sized image.</returns>
-		private static async Task<Image> GetImageAsync(ImageSource source, int height, int width)
-		{
-			var image = new Image();
-			var handler = GetHandler(source);
-			var imageSource = await handler.LoadImageAsync(source);
+            return GetImageAsync(
+                (!sourceButton.IsEnabled && sourceButton.DisabledSource != null) ? sourceButton.DisabledSource : sourceButton.Source,
+                GetHeight(sourceButton.ImageHeightRequest),
+                GetWidth(sourceButton.ImageWidthRequest));
+        }
 
-			image.Source = imageSource;
-			image.Height = Convert.ToDouble(height / 2);
-			image.Width = Convert.ToDouble(width / 2);
-			return image;
-		}
+        /// <summary>
+        ///     Returns the alignment of the label on the button depending on the orientation.
+        /// </summary>
+        /// <param name="imageOrientation">The orientation to use.</param>
+        /// <returns>The alignment to use for the text.</returns>
+        private static TextAlignment GetTextAlignment(ImageOrientation imageOrientation)
+        {
+            TextAlignment returnValue;
+            switch (imageOrientation)
+            {
+                case ImageOrientation.ImageToLeft:
+                    returnValue = TextAlignment.Left;
+                    break;
+                case ImageOrientation.ImageToRight:
+                    returnValue = TextAlignment.Right;
+                    break;
+                default:
+                    returnValue = TextAlignment.Center;
+                    break;
+            }
 
-		/// <summary>
-		///     Sets a margin of 10 between the image and the text.
-		/// </summary>
-		/// <param name="image">The image to add a margin to.</param>
-		/// <param name="orientation">The orientation of the image on the button.</param>
-		private static void SetImageMargin(Image image, ImageOrientation orientation)
-		{
-			const int DefaultMargin = 10;
-			var left = 0;
-			var top = 0;
-			var right = 0;
-			var bottom = 0;
+            return returnValue;
+        }
 
-			switch (orientation)
-			{
-				case ImageOrientation.ImageToLeft:
-					right = DefaultMargin;
-					break;
-				case ImageOrientation.ImageOnTop:
-					bottom = DefaultMargin;
-					break;
-				case ImageOrientation.ImageToRight:
-					left = DefaultMargin;
-					break;
-				case ImageOrientation.ImageOnBottom:
-					top = DefaultMargin;
-					break;
-			}
+        /// <summary>
+        ///     Returns a <see cref="Xamarin.Forms.Image" /> from the <see cref="ImageSource" /> provided.
+        /// </summary>
+        /// <param name="source">The <see cref="ImageSource" /> to load the image from.</param>
+        /// <param name="height">The height for the image (divides by 2 for the Windows Phone platform).</param>
+        /// <param name="width">The width for the image (divides by 2 for the Windows Phone platform).</param>
+        /// <returns>A properly sized image.</returns>
+        private static async Task<Image> GetImageAsync(ImageSource source, int height, int width)
+        {
+            var image = new Image();
+            var handler = GetHandler(source);
+            var imageSource = await handler.LoadImageAsync(source);
 
-			image.Margin = new Thickness(left, top, right, bottom);
-		}
-	}
+            image.Source = imageSource;
+            image.Height = Convert.ToDouble(height / 2);
+            image.Width = Convert.ToDouble(width / 2);
+            return image;
+        }
+
+        /// <summary>
+        ///     Sets a margin of 10 between the image and the text.
+        /// </summary>
+        /// <param name="image">The image to add a margin to.</param>
+        /// <param name="orientation">The orientation of the image on the button.</param>
+        private static void SetImageMargin(Image image, ImageOrientation orientation)
+        {
+            const int DefaultMargin = 10;
+            var left = 0;
+            var top = 0;
+            var right = 0;
+            var bottom = 0;
+
+            switch (orientation)
+            {
+                case ImageOrientation.ImageToLeft:
+                    right = DefaultMargin;
+                    break;
+                case ImageOrientation.ImageOnTop:
+                    bottom = DefaultMargin;
+                    break;
+                case ImageOrientation.ImageToRight:
+                    left = DefaultMargin;
+                    break;
+                case ImageOrientation.ImageOnBottom:
+                    top = DefaultMargin;
+                    break;
+            }
+
+            image.Margin = new Thickness(left, top, right, bottom);
+        }
+    }
 }
--- src/Forms/XLabs.Forms/Controls/ImageButton.cs
+++ src/Forms/XLabs.Forms/Controls/ImageButton.cs
@@ -4,89 +4,117 @@ using Xamarin.Forms;
 
 namespace XLabs.Forms.Controls
 {
-	using XLabs.Enums;
+    using XLabs.Enums;
 
-	/// <summary>
-	/// Creates a button with text and an image.
-	/// The image can be on the left, above, on the right or below the text.
-	/// </summary>
-	public class ImageButton : Button
-	{
-		/// <summary>
-		/// Backing field for the Image property.
-		/// </summary>
-		public static readonly BindableProperty SourceProperty = BindableProperty.Create<ImageButton, ImageSource>((Expression<Func<ImageButton, ImageSource>>)(w => w.Source), (ImageSource)null, BindingMode.OneWay, (BindableProperty.ValidateValueDelegate<ImageSource>)null, (BindableProperty.BindingPropertyChangedDelegate<ImageSource>)((bindable, oldvalue, newvalue) => ((VisualElement)bindable).ToString()), (BindableProperty.BindingPropertyChangingDelegate<ImageSource>)null, (BindableProperty.CoerceValueDelegate<ImageSource>)null);
+    /// <summary>
+    /// Creates a button with text and an image.
+    /// The image can be on the left, above, on the right or below the text.
+    /// </summary>
+    public class ImageButton : Button
+    {
+        /// <summary>
+        /// Backing field for the Image property.
+        /// </summary>
+        public static readonly BindableProperty SourceProperty = BindableProperty.Create(
+            (Expression<Func<ImageButton, ImageSource>>)(w => w.Source), 
+            null, 
+            BindingMode.OneWay, 
+            null, 
+            (bindable, oldvalue, newvalue) => ((VisualElement)bindable).ToString());
 
-		/// <summary>
-		/// Gets or sets the ImageSource to use with the control.
-		/// </summary>
-		/// <value>
-		/// The Source property gets/sets the value of the backing field, SourceProperty.
-		/// </value>
-		[TypeConverter(typeof(ImageSourceConverter))]
-		public ImageSource Source
-		{
-			get { return (ImageSource)GetValue(SourceProperty); }
-			set { SetValue(SourceProperty, value); }
-		}
+        /// <summary>
+        /// Backing field for the Image property.
+        /// </summary>
+        public static readonly BindableProperty DisabledSourceProperty = BindableProperty.Create(
+            (Expression<Func<ImageButton, ImageSource>>)(w => w.DisabledSource),
+            null,
+            BindingMode.OneWay,
+            null,
+            (bindable, oldvalue, newvalue) => ((VisualElement)bindable).ToString());
 
-		/// <summary>
-		/// Backing field for the orientation property.
-		/// </summary>
-		public static readonly BindableProperty OrientationProperty =
-			BindableProperty.Create<ImageButton, ImageOrientation>(
-				p => p.Orientation, ImageOrientation.ImageToLeft);
+        /// <summary>
+        /// Backing field for the image width property.
+        /// </summary>
+        public static readonly BindableProperty ImageWidthRequestProperty =
+            BindableProperty.Create<ImageButton, int>(
+                p => p.ImageWidthRequest, default(int));
 
-		/// <summary>
-		/// Gets or sets The orientation of the image relative to the text.
-		/// </summary> 
-		/// <value>
-		/// The Orientation property gets/sets the value of the backing field, OrientationProperty.
-		/// </value> 
-		public ImageOrientation Orientation
-		{
-			get { return (ImageOrientation)GetValue(OrientationProperty); }
-			set { SetValue(OrientationProperty, value); }
-		}
+        /// <summary>
+        /// Backing field for the image height property.
+        /// </summary>
+        public static readonly BindableProperty ImageHeightRequestProperty =
+            BindableProperty.Create<ImageButton, int>(
+                p => p.ImageHeightRequest, default(int));
 
-		/// <summary>
-		/// Backing field for the image height property.
-		/// </summary>
-		public static readonly BindableProperty ImageHeightRequestProperty =
-			BindableProperty.Create<ImageButton, int>(
-				p => p.ImageHeightRequest, default(int));
+        /// <summary>
+        /// Backing field for the orientation property.
+        /// </summary>
+        public static readonly BindableProperty OrientationProperty =
+            BindableProperty.Create<ImageButton, ImageOrientation>(
+                p => p.Orientation, ImageOrientation.ImageToLeft);
 
-		/// <summary>
-		/// Gets or sets the requested height of the image.  If less than or equal to zero than a 
-		/// height of 50 will be used.
-		/// </summary>
-		/// <value>
-		/// The ImageHeightRequest property gets/sets the value of the backing field, ImageHeightRequestProperty.
-		/// </value> 
-		public int ImageHeightRequest
-		{
-			get { return (int)GetValue(ImageHeightRequestProperty); }
-			set { SetValue(ImageHeightRequestProperty, value); }
-		}
+        /// <summary>
+        /// Gets or sets the ImageSource to use with the control.
+        /// </summary>
+        /// <value>
+        /// The Source property gets/sets the value of the backing field, SourceProperty.
+        /// </value>
+        [TypeConverter(typeof(ImageSourceConverter))]
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
 
-		/// <summary>
-		/// Backing field for the image width property.
-		/// </summary>
-		public static readonly BindableProperty ImageWidthRequestProperty =
-		   BindableProperty.Create<ImageButton, int>(
-			   p => p.ImageWidthRequest, default(int));
+        /// <summary>
+        /// Gets or sets the ImageSource to use with the control.
+        /// </summary>
+        /// <value>
+        /// The Source property gets/sets the value of the backing field, SourceProperty.
+        /// </value>
+        [TypeConverter(typeof(ImageSourceConverter))]
+        public ImageSource DisabledSource
+        {
+            get { return (ImageSource)GetValue(DisabledSourceProperty); }
+            set { SetValue(DisabledSourceProperty, value); }
+        }
 
-		/// <summary>
-		/// Gets or sets the requested width of the image.  If less than or equal to zero than a 
-		/// width of 50 will be used.
-		/// </summary>
-		/// <value>
-		/// The ImageHeightRequest property gets/sets the value of the backing field, ImageHeightRequestProperty.
-		/// </value> 
-		public int ImageWidthRequest
-		{
-			get { return (int)GetValue(ImageWidthRequestProperty); }
-			set { SetValue(ImageWidthRequestProperty, value); }
-		}
-	}
+        /// <summary>
+        /// Gets or sets The orientation of the image relative to the text.
+        /// </summary> 
+        /// <value>
+        /// The Orientation property gets/sets the value of the backing field, OrientationProperty.
+        /// </value> 
+        public ImageOrientation Orientation
+        {
+            get { return (ImageOrientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the requested height of the image.  If less than or equal to zero than a 
+        /// height of 50 will be used.
+        /// </summary>
+        /// <value>
+        /// The ImageHeightRequest property gets/sets the value of the backing field, ImageHeightRequestProperty.
+        /// </value> 
+        public int ImageHeightRequest
+        {
+            get { return (int)GetValue(ImageHeightRequestProperty); }
+            set { SetValue(ImageHeightRequestProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the requested width of the image.  If less than or equal to zero than a 
+        /// width of 50 will be used.
+        /// </summary>
+        /// <value>
+        /// The ImageHeightRequest property gets/sets the value of the backing field, ImageHeightRequestProperty.
+        /// </value> 
+        public int ImageWidthRequest
+        {
+            get { return (int)GetValue(ImageWidthRequestProperty); }
+            set { SetValue(ImageWidthRequestProperty, value); }
+        }
+    }
 }
