316b0634732eb2ac92609483dfa0406d51034a9b
ravensorb <sanderson@eye-catcher.com>
2015-06-06 15:28:04 -0400

    Updated Ninject Nuspec file to handle full .NET platform correctly Updated Refrences to that all are building against consistent versions Initial setup for automating full build using psake build scripts (note these require powershell) Moved version into version.json to make it easier to manage from single location

---
+++ default.ps1
@@ -0,0 +1,74 @@
+properties {
+    $projects = $null
+    $configuration = "Release"
+    $source_folder = $null
+    $solution = $null
+	$build_meta = $null
+	$preRelease = $null
+}
+
+Task Default -Depends Build
+
+Task RestorePackages {
+	Exec { nuget restore $solution -PackagesDirectory .\packages }
+}
+
+Task Publish -Depends Package {
+    $version = getVersionBase
+    
+#    $projects | % {
+#        Get-ChildItem | Where-Object -FilterScript {
+#            ($_.Name.Contains("$project.$version")) -and !($_.Name.Contains(".symbols")) -and ($_.Extension -eq '.nupkg')    
+#        } | % {
+#            exec { nuget push $_.Fullname }
+#        }
+#    }
+	exec { & ".\.nuget\Push All.ps1"}	
+}
+
+Task Package -Depends {
+    $version = getVersionBase
+    
+#    $projects | % {
+#        Get-ChildItem -Path "$_\*.csproj" | % {
+#            exec { nuget pack -sym $_.Fullname -Prop Configuration=$configuration }
+#        }        
+#    }
+	exec { & ".\.nuget\Build All.ps1" "-version $version -preRelease $preRelease"}
+}
+
+Task Test -Depends Build {
+#    Get-ChildItem $source_folder -Recurse -Include *Tests.csproj | % {
+#        Exec { & ".\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe" "$($_.DirectoryName)\bin\$configuration\$($_.BaseName).dll" }
+#    }
+}
+
+Task Build -Depends Clean,Set-Versions,RestorePackages {
+    Exec { msbuild "$solution" /t:Build /p:Configuration=$configuration } 
+}
+
+Task Clean {
+    Exec { msbuild "$solution" /t:Clean /p:Configuration=$configuration } 
+	exec { & ".\.nuget\Clean All.ps1"}
+}
+
+Task Set-Versions {
+    $version = getVersionBase
+
+	if ($build_meta) {
+        "##teamcity[buildNumber '$version+$build_meta']" | Write-Host
+    } else {
+		"##teamcity[buildNumber '$version']" | Write-Host
+	}
+
+    Get-ChildItem -Recurse -Force | Where-Object { $_.Name -eq "AssemblyInfo.cs" } | ForEach-Object {
+        (Get-Content $_.FullName) | ForEach-Object {
+            ($_ -replace 'AssemblyVersion\(.*\)', ('AssemblyVersion("' + $version + '")')) -replace 'AssemblyFileVersion\(.*\)', ('AssemblyFileVersion("' + $version + '")')
+        } | Set-Content $_.FullName -Encoding UTF8
+    }    
+}
+
+function getVersionBase {
+    $versionInfo = (Get-Content "version.json") -join "`n" | ConvertFrom-Json
+    "$($versionInfo.major).$($versionInfo.minor).$($versionInfo.patch)";    
+}
