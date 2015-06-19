169c9ed61db78a6a2f2b2e4feccceceae474e3b6
skallio <skallio@3DMRXN1.bstsoftware.net>
2015-06-19 12:54:02 -0400

    Added delete methods to IFileManager
    https://github.com/XLabs/Xamarin-Forms-Labs/issues/818

--- src/Platform/XLabs.Platform.Shared/Services/IO/FileManager.cs
+++ src/Platform/XLabs.Platform.Shared/Services/IO/FileManager.cs
@@ -4,10 +4,22 @@ using System.IO.IsolatedStorage;
 
 namespace XLabs.Platform.Services.IO
 {
-	public class FileManager : IFileManager
+    public class FileManager : IFileManager
     {
         private readonly IsolatedStorageFile isolatedStorageFile;
 
+        /// <summary>
+        /// Initialized new instance of <see cref="FileManager"/> using user store for application.
+        /// </summary>
+        public FileManager() : this(IsolatedStorageFile.GetUserStoreForApplication())
+        {
+            
+        }
+
+        /// <summary>
+        /// Initialized new instance of <see cref="FileManager"/>.
+        /// </summary>
+        /// <param name="isolatedStorageFile">Isolated storage file to use.</param>
         public FileManager(IsolatedStorageFile isolatedStorageFile)
         {
             this.isolatedStorageFile = isolatedStorageFile;
@@ -25,12 +37,12 @@ namespace XLabs.Platform.Services.IO
 
         public Stream OpenFile(string path, FileMode mode, FileAccess access)
         {
-            return (Stream)this.isolatedStorageFile.OpenFile(path, (System.IO.FileMode)mode, (System.IO.FileAccess)access);
+            return this.isolatedStorageFile.OpenFile(path, (System.IO.FileMode)mode, (System.IO.FileAccess)access);
         }
 
         public Stream OpenFile(string path, FileMode mode, FileAccess access, FileShare share)
         {
-            return (Stream)this.isolatedStorageFile.OpenFile(path, (System.IO.FileMode)mode, (System.IO.FileAccess)access, (System.IO.FileShare)share);
+            return this.isolatedStorageFile.OpenFile(path, (System.IO.FileMode)mode, (System.IO.FileAccess)access, (System.IO.FileShare)share);
         }
 
         public bool FileExists(string path)
@@ -41,6 +53,16 @@ namespace XLabs.Platform.Services.IO
         public DateTimeOffset GetLastWriteTime(string path)
         {
             return this.isolatedStorageFile.GetLastWriteTime(path);
+        }
+
+        public void DeleteFile(string path)
+        {
+            this.isolatedStorageFile.DeleteFile(path);
+        }
+
+        public void DeleteDirectory(string path)
+        {
+            this.isolatedStorageFile.DeleteDirectory(path);
         }
     }
 }
