b9283e97a45ce4ab103fa4682fe6bcea7b1defb3
Sami M. Kallio <code.kallio@gmail.com>
2015-07-13 21:21:20 -0400

    Fixed SecureStorage iOS implementation and added unit testing for the interface

---
+++ UnitTesting/Labs.Tests.Droid/Tests/PreferenceStorageTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Labs.Tests.Droid.Tests
+{
+    using NUnit.Framework;
+    using SecureStorageTests;
+    using XLabs.Platform.Services;
+    using XLabs.Serialization;
+    using XLabs.Serialization.ServiceStack;
+
+    [TestFixture]
+    public class PreferenceStorageTests : SecureStorageTests
+    {
+        #region Overrides of SecureStorageTests
+
+        protected override ISecureStorage Storage
+        {
+            get { return new SharedPreferencesStorage("PassW0rd"); }
+        }
+
+        protected override IByteSerializer Serializer
+        {
+            get { return new JsonSerializer(); }
+        }
+
+        #endregion
+    }
+}
