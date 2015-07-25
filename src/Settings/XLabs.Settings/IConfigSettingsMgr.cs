32a946a5815cafff5c66da9bcd8789687be2f63f
Shawn Anderson <sanderson@eye-catcher.com>
2015-07-25 09:48:44 -0400

    Fix a few build path issues Added new generic settings framework and 2 implementations (Simple and one provider based on XamSettings)

---
+++ src/Settings/XLabs.Settings/IConfigSettingsMgr.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XLabs.Settings
+{
+    public interface IConfigSettingsMgr
+    {
+        /// <summary>
+        /// Gets the setting value. If it is not found, the default value is returned
+        /// </summary>
+        /// <typeparam name="T">the type of the setting</typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The setting or the default value.</returns>
+        T GetValue<T>(string key, T defaultValue = default(T));        
+
+        /// <summary>
+        /// Sets the setting value.
+        /// </summary>
+        /// <typeparam name="T">the type of the setting</typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns><c>true</c> if setting the value was a success, <c>false</c> otherwise.</returns>
+        bool SetValue<T>(string key, T value);
+
+        /// <summary>
+        /// Gets or sets the <see cref="System.Object"/> with the specified setting value.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>System.Object.</returns>
+        object this[string key] { get; set; }
+
+        /// <summary>
+        /// Loads the configuration settings.
+        /// </summary>
+        /// <param name="loadFunc">The optional function to execute during the load process.</param>
+        void Load(Func<IConfigSettingsMgr, bool> loadFunc = null);
+
+        /// <summary>
+        /// Refreshes this instance.
+        /// </summary>
+        //void Refresh();
+
+        /// <summary>
+        /// Flushes this instance.
+        /// </summary>
+        void Flush();
+    }
+}
---
+++ src/Settings/XLabs.Settings/SimpleConfigSettingsMgr.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XLabs.Settings
+{
+    /// <summary>
+    /// Class SimpleConfigSettings.
+    /// </summary>
+    public class SimpleConfigSettingsMgr : IConfigSettingsMgr
+    {
+        /// <summary>
+        /// The _settings
+        /// </summary>
+        private readonly IDictionary<string, object> _settings = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleConfigSettingsMgr"/> class.
+        /// </summary>
+        public SimpleConfigSettingsMgr()
+        {
+            
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleConfigSettingsMgr"/> class.
+        /// </summary>
+        /// <param name="loadFunc">The function load.</param>
+        public SimpleConfigSettingsMgr(Func<IConfigSettingsMgr, bool> loadFunc)
+        {
+            Load(loadFunc);
+        }
+
+        /// <summary>
+        /// Gets the setting value. If it is not found, the default value is returned
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The setting or the default value.</returns>
+        public T GetValue<T>(string key, T defaultValue = default(T))
+        {
+            var result = defaultValue;
+            object tmp = result;
+
+            if (_settings.ContainsKey(key)) tmp = _settings[key];
+
+            result = (T)Convert.ChangeType(tmp, typeof(T));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Sets the setting value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns><c>true</c> if setting the value was a success, <c>false</c> otherwise.</returns>
+        public bool SetValue<T>(string key, T value)
+        {
+            _settings[key] = value;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="System.Object"/> with the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>System.Object.</returns>
+        public object this[string key]
+        {
+            get { return GetValue<object>(key); }
+            set { SetValue(key, value); }
+        }
+
+        /// <summary>
+        /// Loads the configuration settings.
+        /// </summary>
+        /// <param name="loadFunc">The optional function to execute during the load process.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void Load(Func<IConfigSettingsMgr, bool> loadFunc = null)
+        {
+            if (loadFunc != null)
+            {
+                loadFunc(this);
+            }
+        }
+
+        /// <summary>
+        /// Refreshes this instance.
+        /// </summary>
+        //public void Refresh()
+        //{
+        //}
+
+        /// <summary>
+        /// Flushes this instance.
+        /// </summary>
+        public void Flush()
+        {
+        }
+    }
+}
