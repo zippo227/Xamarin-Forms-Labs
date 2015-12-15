24089e9ffb9298fc49176edf806f221005370c68
lewistodd <lewistodd@lewiss-imac.admin.fwbs.net>
2015-12-15 16:23:10 +0000

    Fixed gesture detection for iOS swipe action
    Added logic to capture the active views and split handlers out for each
    type of swipe action -left/right/up/down etc.

--- src/Forms/XLabs.Forms.iOS/Controls/GesturesContentView/GesturesContentViewRenderer.cs
+++ src/Forms/XLabs.Forms.iOS/Controls/GesturesContentView/GesturesContentViewRenderer.cs
@@ -1,6 +1,7 @@
 using Xamarin.Forms;
 
 using XLabs.Forms.Controls;
+using System.Linq;
 
 [assembly: ExportRenderer(typeof(GesturesContentView),typeof(GesturesContentViewRenderer))]
 
@@ -46,7 +47,8 @@ namespace XLabs.Forms.Controls
                     if (x.State == UIGestureRecognizerState.Ended)
                     {
                         var viewpoint = x.LocationInView(this);
-                        this.Element.ProcessGesture(new GestureResult { GestureType = GestureType.SingleTap, Direction = Directionality.None, Origin = viewpoint.ToPoint() });                        
+						this.Element.ProcessGesture(new GestureResult { GestureType = GestureType.SingleTap, Direction = Directionality.None, Origin = viewpoint.ToPoint(),
+							ViewStack = Element.ExcludeChildren ? ViewsContaining(viewpoint.ToPoint()) : null});                        
                     }
                 }){NumberOfTapsRequired = 1});
             //Double Tap
@@ -55,7 +57,8 @@ namespace XLabs.Forms.Controls
                     if (x.State == UIGestureRecognizerState.Ended)
                     {
                         var viewpoint = x.LocationInView(this);
-                        this.Element.ProcessGesture(new GestureResult { GestureType = GestureType.DoubleTap, Direction = Directionality.None, Origin = viewpoint.ToPoint() });                        
+						this.Element.ProcessGesture(new GestureResult { GestureType = GestureType.DoubleTap, Direction = Directionality.None, Origin = viewpoint.ToPoint(),
+							ViewStack = Element.ExcludeChildren ? ViewsContaining(viewpoint.ToPoint()) : null});                        
                     }
                 }) { NumberOfTapsRequired = 2 });
             // Longpress
@@ -64,32 +67,67 @@ namespace XLabs.Forms.Controls
                     if (x.State == UIGestureRecognizerState.Ended)
                     {
                         var viewpoint = x.LocationInView(this);
-                        this.Element.ProcessGesture(new GestureResult { GestureType = GestureType.LongPress, Direction = Directionality.None, Origin = viewpoint.ToPoint() });                        
+						this.Element.ProcessGesture(new GestureResult { GestureType = GestureType.LongPress, Direction = Directionality.None, Origin = viewpoint.ToPoint(),
+							ViewStack = Element.ExcludeChildren ? ViewsContaining(viewpoint.ToPoint()) : null});                        
                     }
                 }));
-            // Swipe
-            this._recognizers.Add(new UISwipeGestureRecognizer((x) =>
-                {
-                    if (x.State == UIGestureRecognizerState.Began)
-                    {
-                        this._swipeStart = x.LocationInView(this).ToPoint();
-                    }
-                    if (x.State == UIGestureRecognizerState.Ended)
-                    {
-                        var endpoint = x.LocationInView(this).ToPoint();
-                        var distance = this._swipeStart.Distance(endpoint);
-                        var distanceX = Math.Abs(this._swipeStart.X - endpoint.X);
-                        var distanceY = Math.Abs(this._swipeStart.Y - endpoint.Y);
-                        var direction = Directionality.None;
-                        direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Left) == UISwipeGestureRecognizerDirection.Left ? Directionality.Left : Directionality.None);
-                        direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Right) == UISwipeGestureRecognizerDirection.Right ? Directionality.Right : Directionality.None);
-                        direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Up) == UISwipeGestureRecognizerDirection.Up ? Directionality.Up : Directionality.None);
-                        direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Down) == UISwipeGestureRecognizerDirection.Down ? Directionality.Down : Directionality.None);
-
-                        this.Element.ProcessGesture(new GestureResult { Direction = direction, GestureType = GestureType.Swipe, HorizontalDistance = distanceX, VerticalDistance = distanceY, Origin = _swipeStart, Length = distance });
-                    }
-                }){ Direction = AllDirections,NumberOfTouchesRequired = 1});
+            // Swipe left
+			this._recognizers.Add(new UISwipeGestureRecognizer(SwipeRecognizer){ Direction =  UISwipeGestureRecognizerDirection.Left, NumberOfTouchesRequired = 1});
+			// Swipe right
+			this._recognizers.Add(new UISwipeGestureRecognizer(SwipeRecognizer){ Direction =  UISwipeGestureRecognizerDirection.Right, NumberOfTouchesRequired = 1});
+			// Swipe up
+			this._recognizers.Add(new UISwipeGestureRecognizer(SwipeRecognizer){ Direction =  UISwipeGestureRecognizerDirection.Up, NumberOfTouchesRequired = 1});
+			// Swipe down
+			this._recognizers.Add(new UISwipeGestureRecognizer(SwipeRecognizer){ Direction =  UISwipeGestureRecognizerDirection.Down, NumberOfTouchesRequired = 1});
         }
+
+		void SwipeRecognizer(UISwipeGestureRecognizer x)
+		{
+			if (x.State == UIGestureRecognizerState.Began)
+			{
+				this._swipeStart = x.LocationInView(this).ToPoint();
+			}
+			if (x.State == UIGestureRecognizerState.Ended)
+			{
+				var endpoint = x.LocationInView(this).ToPoint();
+				var distance = this._swipeStart.Distance(endpoint);
+				var distanceX = Math.Abs(this._swipeStart.X - endpoint.X);
+				var distanceY = Math.Abs(this._swipeStart.Y - endpoint.Y);
+				var direction = Directionality.None;
+				direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Left) == UISwipeGestureRecognizerDirection.Left ? Directionality.Left : Directionality.None);
+				direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Right) == UISwipeGestureRecognizerDirection.Right ? Directionality.Right : Directionality.None);
+				direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Up) == UISwipeGestureRecognizerDirection.Up ? Directionality.Up : Directionality.None);
+				direction |= ((x.Direction & UISwipeGestureRecognizerDirection.Down) == UISwipeGestureRecognizerDirection.Down ? Directionality.Down : Directionality.None);
+
+				this.Element.ProcessGesture(new GestureResult { Direction = direction, GestureType = GestureType.Swipe, HorizontalDistance = distanceX, VerticalDistance = distanceY, Origin = _swipeStart, Length = distance, 
+					ViewStack = Element.ExcludeChildren ? ViewsContaining(_swipeStart) : null});
+			}
+		}
+
+		private List<View> ViewsContaining(Point pt)
+		{
+			var ret = new List<View>();
+			return ViewsContainingImpl(pt, ret, this).OrderByDescending(x=>x.Width * x.Height).ToList();
+		}
+
+		private IEnumerable<View> ViewsContainingImpl(Point pt,List<View>views, UIView root )
+		{
+			if (root != null && root.Subviews != null) 
+			{
+				for (var i = 0; i < root.Subviews.Count (); i++) 
+				{
+					var child = root.Subviews [i];
+					var bounds = child.Bounds;
+					var ver = child as IVisualElementRenderer;
+					if (ver == null || !(ver.Element is View) || !bounds.Contains (Convert.ToInt32 (pt.X), Convert.ToInt32 (pt.Y)))
+						continue;
+					views.Add (ver.Element as View);
+					//check to see if this containing child has any other containing children
+					ViewsContainingImpl (pt, views, child);
+				}
+			}
+			return views;
+		}
 
         /// <summary>When the underlying element is changed we detach from the old, and attach to the new</summary>
         /// <param name="e">The <see cref="ElementChangedEventArgs{GesturesContentView}"/> instance containing the event data.</param>
